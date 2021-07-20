df961f295e6ed6fc554e8b7e05f4d729d78a5ef9
fitrh <fitrahm17h@student.unhas.ac.id>
2021-07-20 22:27:49 +0800

    [ plugin/outline ] use setup function instead of global variable
    Use setup function from this plugin to pass the configuration so if we
    using packer lazy loading feature, we can put the configuration in
    config[1] key instead define the configuration under global variable.
    
    [1]: config key in packer loaded after the plugin is loaded

--- lua/plugin/outline/config.lua
+++ lua/plugin/outline/config.lua
@@ -1,4 +1,6 @@
-vim.g.symbols_outline = {
+local outline = require("symbols-outline")
+
+outline.setup({
   highlight_hovered_item = true,
   show_guides = true,
   auto_preview = true,
@@ -15,8 +17,35 @@ vim.g.symbols_outline = {
     code_actions = "a",
   },
   lsp_blacklist = {},
-}
+  symbols = {
+    File = { icon = "", hl = "TSURI" },
+    Module = { icon = "", hl = "TSNamespace" },
+    Namespace = { icon = "", hl = "TSNamespace" },
+    Package = { icon = "", hl = "TSNamespace" },
+    Class = { icon = "", hl = "TSType" },
+    Method = { icon = "", hl = "TSMethod" },
+    Property = { icon = "襁", hl = "TSMethod" },
+    Field = { icon = "ﴲ", hl = "TSField" },
+    Constructor = { icon = "", hl = "TSConstructor" },
+    Enum = { icon = "練", hl = "TSType" },
+    Interface = { icon = "ﰮ", hl = "TSType" },
+    Function = { icon = "", hl = "TSFunction" },
+    Variable = { icon = "", hl = "TSConstant" },
+    Constant = { icon = "ﲀ", hl = "TSConstant" },
+    String = { icon = "", hl = "TSString" },
+    Number = { icon = "", hl = "TSNumber" },
+    Boolean = { icon = "⊨", hl = "TSBoolean" },
+    Array = { icon = "", hl = "TSConstant" },
+    Object = { icon = "⦿", hl = "TSType" },
+    Key = { icon = "", hl = "TSType" },
+    Null = { icon = "ﳠ", hl = "TSType" },
+    EnumMember = { icon = "", hl = "TSField" },
+    Struct = { icon = "", hl = "TSType" },
+    Event = { icon = "ﯓ", hl = "TSType" },
+    Operator = { icon = "璉", hl = "TSOperator" },
+    TypeParameter = { icon = "", hl = "TSParameter" }
+  },
+})
 
-vim.api.nvim_set_keymap(
-  "n", "<F8>", "<Cmd>SymbolsOutline<CR>", { noremap = false }
-)
+local set_keymap = vim.api.nvim_set_keymap
+set_keymap("n", "<F8>", "<Cmd>SymbolsOutline<CR>", { noremap = false })
