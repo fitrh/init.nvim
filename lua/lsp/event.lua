aa776e93420f22dcca5db8fb335fad5ddbaacdde
fitrh <fitrahm17h@student.unhas.ac.id>
2021-07-23 03:05:43 +0800

    [ lsp ] Split the event to make it more flexible

--- lua/lsp/event.lua
+++ lua/lsp/event.lua
@@ -1,18 +1,25 @@
-require("util.event.augroup").setup({
-  ["HighlightOnCursor"] = {
-    filter = "<buffer>",
-    {
-      events = "CursorHold",
-      cmd = "lua vim.lsp.buf.document_highlight()"
-    },
-    {
-      events = "CursorHoldI",
-      cmd = "lua vim.lsp.buf.document_highlight()"
-    },
-    {
-      events = "CursorMoved",
-      cmd = "lua vim.lsp.buf.clear_references()"
-    },
-  },
-})
+local M = {}
 
+local augroup = require("util.event.augroup")
+
+function M.highlight_on_cursor_hold()
+  augroup.setup({
+    ["HighlightOnCursor"] = {
+      filter = "<buffer>",
+      {
+        events = "CursorHold",
+        cmd = "lua vim.lsp.buf.document_highlight()"
+      },
+      {
+        events = "CursorHoldI",
+        cmd = "lua vim.lsp.buf.document_highlight()"
+      },
+      {
+        events = "CursorMoved",
+        cmd = "lua vim.lsp.buf.clear_references()"
+      },
+    },
+  })
+end
+
+return M
