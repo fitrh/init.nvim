78845fa8c68c8db4341adf7120c6bd233078bab2
fitrh <fitrahm17h@student.unhas.ac.id>
2021-07-28 12:54:16 +0800

    [ lsp/clangd ] Add comment about clang-tidy

--- lua/lsp/config/clangd.lua
+++ lua/lsp/config/clangd.lua
@@ -7,6 +7,9 @@ local M = {
     -- see clangd --help-hidden
     "clangd",
     "--background-index",
+    -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+    -- to add more checks, create .clang-tidy file in the root directory
+    -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
     "--clang-tidy",
     "--completion-style=bundled",
     "--cross-file-rename",
