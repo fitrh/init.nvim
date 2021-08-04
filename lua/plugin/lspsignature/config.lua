dd71fc76f601ea43571a2b893e5c5db9fc8a231b
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-05 00:47:01 +0800

    [ plugin/lspsignature ] Add `zindex` options
    Addition options from this commit[1].
    
    [1]: https://github.com/ray-x/lsp_signature.nvim/commit/d2c826272804335a4de3dc316ba483a0fc0951c9

--- lua/plugin/lspsignature/config.lua
+++ lua/plugin/lspsignature/config.lua
@@ -5,6 +5,7 @@ local M = {
   hint_scheme = "LspDiagnosticsDefaultHint",
   hi_parameter = "LspSignatureActiveParameter",
   extra_trigger_chars = { "(", "{", "," },
+  zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
 }
 
 return M
