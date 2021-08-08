3e0783d5905e73d483b18a57e3d3c4e6534d8fdd
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-09 02:27:54 +0800

    [ plugin/jdtls ] Add jdtls related configuration

--- lua/plugin/jdtls/config.lua
+++ lua/plugin/jdtls/config.lua
@@ -28,17 +28,47 @@ local root_files = {
 
 local workspace = ("%s/%s"):format(env.WORKSPACE, env.BASENAME_CWD)
 
+local runtimes = {
+  { name = "JavaSE-11", path = ("%s/jdk-11"):format(env.RUNTIMES) },
+  { name = "JavaSE-16", path = ("%s/latest"):format(env.RUNTIMES) },
+}
+
+local settings = {
+  java = { -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+    codeGeneration = {
+      generateComments = true,
+      hashCodeEquals = {
+        useInstanceof = true,
+        useJava7Objects = true,
+      },
+    },
+    configuration = { runtimes = runtimes, },
+    referencesCodeLens = { enabled = true },
+    signatureHelp = { enabled = true },
+    implementationsCodeLens = { enabled = true },
+    saveActions = { organizeImports = true },
+  }
+}
+
 local config = {
   root_dir = setup.find_root(root_files),
   cmd = { "jdtls", workspace, },
   capabilities = capabilities,
   handlers = handler.default(),
+  settings = settings,
+  init_options = {
+    extendedClientCapabilities = jdtls_capability;
+  },
+  on_init = function(client, _)
+    client.notify('workspace/didChangeConfiguration', { settings = settings })
+  end,
   on_attach = function(client, bufnr)
     setup.add_commands()
     attach.with_all_extensions(client, bufnr)
   end,
   flags = {
     allow_incremental_sync = true,
+    server_side_fuzzy_completion = true,
   },
 }
 
