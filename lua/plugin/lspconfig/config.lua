56ff3334bc1912416d507ffddd0d3f6049972a85
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-10 17:28:35 +0800

    [ plugin/lspconfig ] Add `hls`, the haskell language server

--- lua/plugin/lspconfig/config.lua
+++ lua/plugin/lspconfig/config.lua
@@ -1,8 +1,9 @@
 local lsp = require("lspconfig")
 
 local servers = {
-  clangd = require("lsp.config.clangd"), -- FIXME: properly configure clangd
+  clangd = require("lsp.config.clangd"),
   gopls = require("lsp.config.gopls"),
+  hls = require("lsp.config.hls"),
   pyright = require("lsp.config.pyright"),
   bashls = require("lsp.config.bashls"),
   -- TODO: add null-ls for markdown and toml(?)
