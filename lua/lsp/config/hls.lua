e120d74185408e8caa77acc297b716c7185d8513
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-10 17:28:03 +0800

    [ lsp/config/hls ] Initial configuration for `hls`

---
+++ lua/lsp/config/hls.lua
@@ -0,0 +1,15 @@
+local handler = require("lsp.handler")
+local attach = require("lsp.attach")
+local capabilities = require("lsp.capability")
+
+return {
+  capabilities = capabilities,
+  handlers = handler.default(),
+  settings = {
+    haskell = {
+      formattingProvider = "brittanty",
+      maxCompletions = 10,
+    }
+  },
+  on_attach = attach.with_all_extensions,
+}
