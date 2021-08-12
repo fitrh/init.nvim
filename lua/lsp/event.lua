9c3388e0c2be708436ff12b9ae790850774072ab
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-13 04:27:29 +0800

    [ lsp/event ] Check client capabilites first, 93d7b01 follow up
    Make use of the new `expect` key from `util/event/augroup`

--- lua/lsp/event.lua
+++ lua/lsp/event.lua
@@ -2,9 +2,10 @@ local M = {}
 
 local augroup = require("util.event.augroup")
 
-function M.highlight_on_cursor_hold()
+function M.attach(client)
   augroup.setup({
     ["HighlightOnCursor"] = {
+      expect = client.resolved_capabilities.document_highlight,
       filter = "<buffer>",
       {
         events = "CursorHold",
@@ -18,6 +19,12 @@ function M.highlight_on_cursor_hold()
         events = "CursorMoved",
         cmd = "lua vim.lsp.buf.clear_references()"
       },
+    },
+    ["CodelensOnInsert"] = {
+      expect = client.resolved_capabilities.code_lens,
+      events = "BufEnter,CursorHold,InsertEnter,InsertLeave",
+      filter = "<buffer>",
+      { cmd = "lua vim.lsp.codelens.refresh()" }
     },
   })
 end
