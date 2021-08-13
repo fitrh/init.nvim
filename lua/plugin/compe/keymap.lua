5f496dc8da59983c8e09b1c34a1525bcb12e88c5
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-13 17:06:26 +0800

    [ plugin/compe ] Update keymap to automatically select the first match

--- lua/plugin/compe/keymap.lua
+++ lua/plugin/compe/keymap.lua
@@ -8,10 +8,7 @@ keymap.bind({
   on("<S-Tab>", "i"):run("v:lua.s_tab_complete()"):with(opt():expr()),
   on("<S-Tab>", "s"):run("v:lua.s_tab_complete()"):with(opt():expr()),
   on("<C-Space>", "i"):run("compe#complete()"):with(opt():noremap():expr()),
-  -- on("<CR>", "i"):run("compe#confirm('<CR>')"):with(opt():expr()),
-  -- on("<CR>", "i"):run(
-  --   [[compe#confirm(luaeval('require "nvim-autopairs".autopairs_cr()'))]]
-  -- ):with(opt():expr()),
+  on("<CR>", "i"):run("compe#confirm({ 'keys': '<CR>', 'select': v:true })"):with(opt():expr()),
   on("<C-e>", "i"):run("compe#close('<C-e>')"):with(opt():expr()),
   on("<C-f>", "i"):run("compe#scroll({ 'delta': +4 })"):with(opt():expr()),
   on("<C-d>", "i"):run("compe#scroll({ 'delta': -4 })"):with(opt():expr()),
