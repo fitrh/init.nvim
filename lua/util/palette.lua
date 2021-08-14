86e5a3d780219297e991e85e46089ce7b7151440
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-15 04:10:20 +0800

    [ util/palette ] Add colors

--- lua/util/palette.lua
+++ lua/util/palette.lua
@@ -11,18 +11,24 @@ local M = {
   red = scheme.red,
   red_br_2 = util.brighten(scheme.red, 0.2),
   red1 = scheme.red1,
+  orange = scheme.orange,
   blue = scheme.blue,
   blue2 = scheme.blue2,
   blue5 = scheme.blue5,
   teal = scheme.teal,
   cyan = scheme.cyan,
   purple = scheme.purple,
+  magenta = scheme.magenta,
   gitsigns = {
     add = scheme.gitSigns.add,
     change = scheme.gitSigns.change,
     delete = scheme.gitSigns.delete,
   },
+  fg = scheme.fg,
+  fg_dark = scheme.fg_dark,
   bg_statusline = scheme.bg_statusline,
+  bg_highlight = scheme.bg_highlight,
+  bg_visual = scheme.bg_visual,
 }
 
 return M
