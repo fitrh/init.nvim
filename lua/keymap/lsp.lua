ab4f802e39089a402a9d58c241f6f2cfdf145a65
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-15 22:20:21 +0800

    [ keymap ] Pass buffer number on `bind`, aa6b8b5 follow up

--- lua/keymap/lsp.lua
+++ lua/keymap/lsp.lua
@@ -1,31 +1,31 @@
 local M = {}
 
-function M.on_attach(client, nr)
+function M.on_attach(client, bufnr)
   local keymap = require("util.keymap")
   local on = keymap.on_press
   local lead = keymap.on_press_leader
   local opt = keymap.opt
 
   keymap.bind({
-    on("gD", "n", nr):lua("vim.lsp.buf.declaration()"):with(opt():noremap()),
-    on("gd", "n", nr):lua("vim.lsp.buf.definition()"):with(opt():noremap()),
-    on("gi", "n", nr):lua("vim.lsp.buf.implementation()"):with(opt():noremap()),
-    on("gr", "n", nr):lua("vim.lsp.buf.references()"):with(opt():noremap()),
-    on("K", "n", nr):lua("vim.lsp.buf.hover()"):with(opt():noremap()),
-    lead("k", "n", nr):lua("vim.lsp.buf.signature_help()"):with(opt():noremap()),
-    lead("ls", "n", nr):lua("vim.lsp.buf.document_symbol()"):with(opt():noremap()),
-    lead("wa", "n", nr):lua("vim.lsp.buf.add_workspace_folder()"):with(opt():noremap()),
-    lead("wr", "n", nr):lua("vim.lsp.buf.remove_workspace_folder()"):with(opt():noremap()),
-    lead("D", "n", nr):lua("vim.lsp.buf.type_definition()"):with(opt():noremap()),
-    lead("rn", "n", nr):lua("vim.lsp.buf.rename()"):with(opt():noremap()),
-    lead("ca", "n", nr):lua("vim.lsp.buf.code_action()"):with(opt():noremap()),
-    lead("d", "n", nr):lua("vim.lsp.diagnostic.show_line_diagnostics({ focusable = false, border = 'rounded' })"):with(opt():noremap()),
-    on("[d", "n", nr):lua("vim.lsp.diagnostic.goto_prev({ popup_opts = { focusable = false, border='rounded' } })"):with(opt():noremap()),
-    on("]d", "n", nr):lua("vim.lsp.diagnostic.goto_next({ popup_opts = { focusable = false, border='rounded' } })"):with(opt():noremap()),
-    lead("ll", "n", nr):lua("vim.lsp.diagnostic.set_loclist()"):with(opt():noremap()),
-    lead("lo", nr):exec("Telescope lsp_document_symbols"):with(opt():noremap()),
+    on("gD", "n"):lua("vim.lsp.buf.declaration()"):with(opt():noremap()),
+    on("gd", "n"):lua("vim.lsp.buf.definition()"):with(opt():noremap()),
+    on("gi", "n"):lua("vim.lsp.buf.implementation()"):with(opt():noremap()),
+    on("gr", "n"):lua("vim.lsp.buf.references()"):with(opt():noremap()),
+    on("K", "n"):lua("vim.lsp.buf.hover()"):with(opt():noremap()),
+    lead("k", "n"):lua("vim.lsp.buf.signature_help()"):with(opt():noremap()),
+    lead("ls", "n"):lua("vim.lsp.buf.document_symbol()"):with(opt():noremap()),
+    lead("wa", "n"):lua("vim.lsp.buf.add_workspace_folder()"):with(opt():noremap()),
+    lead("wr", "n"):lua("vim.lsp.buf.remove_workspace_folder()"):with(opt():noremap()),
+    lead("D", "n"):lua("vim.lsp.buf.type_definition()"):with(opt():noremap()),
+    lead("rn", "n"):lua("vim.lsp.buf.rename()"):with(opt():noremap()),
+    lead("ca", "n"):lua("vim.lsp.buf.code_action()"):with(opt():noremap()),
+    lead("d", "n"):lua("vim.lsp.diagnostic.show_line_diagnostics({ focusable = false, border = 'rounded' })"):with(opt():noremap()),
+    on("[d", "n"):lua("vim.lsp.diagnostic.goto_prev({ popup_opts = { focusable = false, border='rounded' } })"):with(opt():noremap()),
+    on("]d", "n"):lua("vim.lsp.diagnostic.goto_next({ popup_opts = { focusable = false, border='rounded' } })"):with(opt():noremap()),
+    lead("ll", "n"):lua("vim.lsp.diagnostic.set_loclist()"):with(opt():noremap()),
+    lead("lo"):exec("Telescope lsp_document_symbols"):with(opt():noremap()),
     -- TODO: add more telescope lsp command
-  })
+  }, bufnr)
 end
 
 return M
--- lua/plugin/gitsigns/keymap.lua
+++ lua/plugin/gitsigns/keymap.lua
@@ -22,31 +22,31 @@ local toggle = {
   blame_win = "Gitsigns blame_line true",
 }
 
-function M.on_attach(nr)
+function M.on_attach(bufnr)
   local keymap = require("util.keymap")
   local on = keymap.on_press
   local lead = keymap.on_press_leader
   local opt = keymap.opt
 
   keymap.bind({
-    on("]c", "n", nr):exec(hunk.nex):with(opt():noremap()),
-    on("[c", "n", nr):exec(hunk.prev):with(opt():noremap()),
-    on("ghs", "n", nr):exec(hunk.stage):with(opt():noremap()),
-    on("ghs", "v", nr):lua(hunk.stages):with(opt():noremap()),
-    on("ghu", "n", nr):exec(hunk.undo):with(opt():noremap()),
-    on("ghr", "n", nr):exec(hunk.reset):with(opt():noremap()),
-    on("ghr", "v", nr):lua(hunk.resetl):with(opt():noremap()),
-    on("ghR", "n", nr):exec(hunk.reset_all):with(opt():noremap()),
-    on("ghp", "n", nr):exec(hunk.preview):with(opt():noremap()),
-    on("ih", "o", nr):exec(hunk.select):with(opt():noremap()),
-    on("ih", "x", nr):exec(hunk.select):with(opt():noremap()),
-    on("gbl", "n", nr):exec(toggle.blame_win):with(opt():noremap()),
-    on("gt", "n", nr):exec(toggle.sign):with(opt():noremap()),
-    on("gnm", "n", nr):exec(toggle.numhl):with(opt():noremap()),
-    on("gll", "n", nr):exec(toggle.linehl):with(opt():noremap()),
-    on("gwd", "n", nr):exec(toggle.word_diff):with(opt():noremap()),
-    lead("gl", "n", nr):exec(toggle.blame):with(opt():noremap()),
-  })
+    on("]c", "n"):exec(hunk.nex):with(opt():noremap()),
+    on("[c", "n"):exec(hunk.prev):with(opt():noremap()),
+    on("ghs", "n"):exec(hunk.stage):with(opt():noremap()),
+    on("ghs", "v"):lua(hunk.stages):with(opt():noremap()),
+    on("ghu", "n"):exec(hunk.undo):with(opt():noremap()),
+    on("ghr", "n"):exec(hunk.reset):with(opt():noremap()),
+    on("ghr", "v"):lua(hunk.resetl):with(opt():noremap()),
+    on("ghR", "n"):exec(hunk.reset_all):with(opt():noremap()),
+    on("ghp", "n"):exec(hunk.preview):with(opt():noremap()),
+    on("ih", "o"):exec(hunk.select):with(opt():noremap()),
+    on("ih", "x"):exec(hunk.select):with(opt():noremap()),
+    on("gbl", "n"):exec(toggle.blame_win):with(opt():noremap()),
+    on("gt", "n"):exec(toggle.sign):with(opt():noremap()),
+    on("gnm", "n"):exec(toggle.numhl):with(opt():noremap()),
+    on("gll", "n"):exec(toggle.linehl):with(opt():noremap()),
+    on("gwd", "n"):exec(toggle.word_diff):with(opt():noremap()),
+    lead("gl", "n"):exec(toggle.blame):with(opt():noremap()),
+  }, bufnr)
 end
 
 return M
