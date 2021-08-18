4fdc42e05283d96e1fe4a55245c2caac08034ce3
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-19 05:24:59 +0800

    [ plugin/sandwich ] Update init key
    - Specify map mode for each key
    
    - Add textobj key (`ib`, `ab`, `is`, `as`, `it`, `at`)

--- lua/plugin/sandwich/init.lua
+++ lua/plugin/sandwich/init.lua
@@ -1,5 +1,21 @@
 return {
   "machakann/vim-sandwich",
   config = function() require("plugin.sandwich.config") end,
-  keys = { "sa", "sr", "sd", },
+  keys = {
+    -- basic add, replace, and delete operator
+    { "n", "sa" },
+    { "v", "sa" },
+    { "n", "sr" },
+    { "v", "sr" },
+    { "n", "sd" },
+    { "v", "sd" },
+    -- textobj
+    { "v", "ib" },
+    { "v", "is" },
+    { "v", "ab" },
+    { "v", "as" },
+    -- textobj tag
+    { "v", "it" },
+    { "v", "at" },
+  },
 }
