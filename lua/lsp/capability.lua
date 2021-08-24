e9bc24d46b4f42f47b2fece793dbe490702e5730
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-24 23:31:46 +0800

    [ lsp/capability ] Remove `documentationFormat` override
    Let the client decide what kind of format should be used.

--- lua/lsp/capability.lua
+++ lua/lsp/capability.lua
@@ -6,7 +6,6 @@ M.window.workDoneProgress = true
 -- https://microsoft.github.io/language-server-protocol/specifications/specification-3-17/#completionClientCapabilities
 M.textDocument.completion.completionItem.snippetSupport = true
 M.textDocument.completion.completionItem.commitCharactersSupport = true
-M.textDocument.completion.completionItem.documentationFormat = { "markdown", }
 M.textDocument.completion.completionItem.deprecatedSupport = true
 M.textDocument.completion.completionItem.preselectSupport = true
 M.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 }, }
