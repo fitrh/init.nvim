f3c24866e181e353ad3f7ce7ba63499dbbb20ebc
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-25 12:44:54 +0800

    [ lsp ] Initial configuration for `cssls` and `html` language server

--- lua/plugin/lspconfig/config.lua
+++ lua/plugin/lspconfig/config.lua
@@ -1,11 +1,13 @@
 local lsp = require("lspconfig")
 
 local servers = {
+  bashls = require("lsp.config.bashls"),
   clangd = require("lsp.config.clangd"),
+  cssls = require("lsp.config.cssls"),
   gopls = require("lsp.config.gopls"),
   hls = require("lsp.config.hls"),
+  html = require("lsp.config.html"),
   pyright = require("lsp.config.pyright"),
-  bashls = require("lsp.config.bashls"),
   -- TODO: add null-ls for markdown and toml(?)
 }
 
