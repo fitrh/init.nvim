5799616a659f78eac5aa9756c539f6d419a6e94a
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-26 18:07:03 +0800

    [ lsp ] Use the default config for `html`, `cssls`, and `bashls`

--- lua/lsp/config/cssls.lua
+++
@@ -1,9 +0,0 @@
-local handler = require("lsp.handler")
-local attach = require("lsp.attach")
-local capabilities = require("lsp.capability")
-
-return {
-  on_attach = attach.with_all_extensions,
-  capabilities = capabilities,
-  handlers = handler.default(),
-}
--- lua/plugin/lspconfig/config.lua
+++ lua/plugin/lspconfig/config.lua
@@ -1,12 +1,12 @@
 local lsp = require("lspconfig")
 
 local servers = {
-  bashls = require("lsp.config.bashls"),
+  bashls = require("lsp.config"),
   clangd = require("lsp.config.clangd"),
-  cssls = require("lsp.config.cssls"),
+  cssls = require("lsp.config"),
   gopls = require("lsp.config.gopls"),
   hls = require("lsp.config.hls"),
-  html = require("lsp.config.html"),
+  html = require("lsp.config"),
   pyright = require("lsp.config.pyright"),
   sumneko_lua = require("lsp.config.sumneko_lua"),
   -- TODO: add null-ls for markdown and toml(?)
