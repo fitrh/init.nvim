25c329247fec40ead4db2799fbd2a9aec5e3d992
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-28 15:50:45 +0800

    [ plugin/hop ] Add `HopLineStart` to `cmd` spec

--- lua/plugin/hop/init.lua
+++ lua/plugin/hop/init.lua
@@ -1,5 +1,12 @@
 return {
   "phaazon/hop.nvim",
   config = function() require("plugin.hop.config") end,
-  cmd = { "HopWord", "HopLine", "HopChar1", "HopChar2", "HopPattern" },
+  cmd = {
+    "HopWord",
+    "HopLine",
+    "HopLineStart",
+    "HopChar1",
+    "HopChar2",
+    "HopPattern"
+  },
 }
