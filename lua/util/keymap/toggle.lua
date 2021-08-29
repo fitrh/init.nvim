afa214cfc4345613a3d3fbfaa1e9db4919d4318a
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-29 14:32:04 +0800

    [ util/keymap/toggle ] Fix `vim.opt` usage

--- lua/util/keymap/toggle.lua
+++ lua/util/keymap/toggle.lua
@@ -3,20 +3,16 @@ local M = {}
 function M.zen(args)
   local opts = args or {}
   local opt = vim.opt
-  local signcolumn = opt.signcolumn
-  local number = opt.number
-  local relativenumber = opt.relativenumber
-  local laststatus = opt.laststatus
 
-  if (signcolumn:get() == "yes") then
-    signcolumn = "no"
-    number = false
-    relativenumber = false
-    if opts.laststatus then laststatus = 0 end
+  if (opt.signcolumn:get() == "yes") then
+    opt.signcolumn = "no"
+    opt.number = false
+    opt.relativenumber = false
+    if opts.laststatus then opt.laststatus = 0 end
   else
-    signcolumn = "yes"
-    number = true
-    relativenumber = true
+    opt.signcolumn = "yes"
+    opt.number = true
+    opt.relativenumber = true
     if opts.laststatus then vim.opt.laststatus = 2 end
   end
 end
