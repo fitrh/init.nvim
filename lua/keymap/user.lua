1a97a958aa27135403121d5f26938d7e89317d29
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-29 14:36:40 +0800

    [ keymap ] Add `user.lua` for keymap that use user defined functionality

---
+++ lua/keymap/user.lua
@@ -0,0 +1,18 @@
+local keymap = require("util.keymap")
+local on = keymap.on_press
+local lead = keymap.on_press_leader
+local opt = keymap.opt
+
+keymap.bind({
+
+  -- Minimal tmux Integration behavior
+  on("<M-h>", "n"):lua([[require("util.tmux").move("h")]]),
+  on("<M-j>", "n"):lua([[require("util.tmux").move("j")]]),
+  on("<M-k>", "n"):lua([[require("util.tmux").move("k")]]),
+  on("<M-l>", "n"):lua([[require("util.tmux").move("l")]]),
+
+  -- Minimal zen mode behavior
+  lead("zz", "n"):lua('require("util.keymap.toggle").zen({ laststatus = true })'),
+  lead("nr", "n"):lua('require("util.keymap.toggle").zen()'),
+
+}, { options = opt():noremap() })
