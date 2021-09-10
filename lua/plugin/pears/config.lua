d55a0c4def48b4dcb07ed2c51694db4b8e872dd8
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-10 22:53:45 +0800

    [ plugin/pears ] Remove `compe` `<CR>` handling

--- lua/plugin/pears/config.lua
+++ lua/plugin/pears/config.lua
@@ -4,14 +4,6 @@ local rule = require("pears.rule")
 pears.setup(function(conf)
   conf.preset "tag_matching"
   conf.remove_pair_on_outer_backspace(false)
-  conf.on_enter(function(pears_handle)
-    if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
-      return vim.fn["compe#confirm"]("{ 'keys': '<CR>', 'select': v:true }")
-    else
-      pears_handle()
-    end
-  end)
-  -- Don't expand a double quote if it comes after an alpha character
   conf.pair([["]], {
     close = [["]],
     should_expand = rule.not_(rule.start_of_context "[a-zA-Z]")
