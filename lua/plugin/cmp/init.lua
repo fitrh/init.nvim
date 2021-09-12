f4fc366f9ab64c8105ae65982f7339a3b965a459
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-12 10:36:30 +0800

    [ plugin/cmp ] Lazy load each sources using `after` spec
    Lazy loading sources using `ft` spec cause a problem *cmp module not
    found* when opening a file directly (trigger the `FileType`, and it is
    always before `InsertEnter`) because we lazy load the `nvim-cmp` on
    `InsertEnter` event, meanwhile, the sources assume that the `nvim-cmp`
    already loaded.

--- lua/plugin/cmp/init.lua
+++ lua/plugin/cmp/init.lua
@@ -3,7 +3,7 @@ return {
   requires = {
     { "hrsh7th/cmp-buffer", after = "nvim-cmp", },
     { "hrsh7th/cmp-path", after = "nvim-cmp", },
-    { "hrsh7th/cmp-nvim-lua", ft = "lua", },
+    { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp", },
     { "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp", },
     { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp", },
   },
