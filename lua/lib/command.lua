578f702982418d54414817232cdf57a0fec8d1e4
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-15 12:58:13 +0800

    [ lib/command ] Add `group`, group user commands with same prefix
    It is actually call the `create` function to create commands that have
    same prefix and group them together.
    
    The `args` is:
    {
        prefix = string, the prefix of the commands,
        option = string (optional), the global option tu use by commands,
        create = table, list of commands, the item have same format with
        the args to pass for `create`
    }

--- lua/lib/command.lua
+++ lua/lib/command.lua
@@ -11,4 +11,21 @@ function M.create(args)
   vim.cmd(cmd)
 end
 
+function M.group(args)
+  local opts = args or {}
+  local prefix = opts.prefix or nil
+  local option = opts.option or nil
+  local create = opts.create or {}
+  for _, v in ipairs(create) do
+    local name = prefix
+        and ("%s%s"):format(
+          str.ufirst(prefix),
+          str.ufirst(v.name or "")
+        )
+      or str.ufirst(v.name)
+    option = option or v.option
+    M.create({ option = option, name = name, cmd = v.cmd })
+  end
+end
+
 return M
