15eec72fc8afe20b81db366f5fcf07b3825dfadc
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-15 22:51:30 +0800

    [ plugin/jdtls ] Use `group` for user command

--- lua/plugin/jdtls/command.lua
+++ lua/plugin/jdtls/command.lua
@@ -1,62 +1,49 @@
-local command = require("lib.command")
-
 local M = {}
 
 function M.attach(client, bufnr)
-  command.create({
-    name = "JdtCodeAction",
+  require("lib.command").group({
+    prefix = "Jdt",
     option = "-buffer",
-    cmd = [[lua require("jdtls").code_action()]]
-  })
-  command.create({
-    name = "JdtCodeActionRange",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").code_action(true)]]
-  })
-  command.create({
-    name = "JdtCodeActionRefactor",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").code_action(false, "refactor")]]
-  })
-  command.create({
-    name = "JdtOrganizeImports",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").organize_imports()]]
-  })
-  command.create({
-    name = "JdtExtractVariable",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").extract_variable()]]
-  })
-  command.create({
-    name = "JdtExtractVariableRange",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").extract_variable(true)]]
-  })
-  command.create({
-    name = "JdtExtractConstant",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").extract_constant()]]
-  })
-  command.create({
-    name = "JdtExtractConstantRange",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").extract_constant(true)]]
-  })
-  command.create({
-    name = "JdtExtractMethod",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").extract_method(true)]]
-  })
-  command.create({
-    name = "JdtTestClass",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").test_class(true)]]
-  })
-  command.create({
-    name = "JdtTestNearestMethod",
-    option = "-buffer",
-    cmd = [[lua require("jdtls").test_nearest_method(true)]]
+    create = {
+      { name = "CodeAction", cmd = [[lua require("jdtls").code_action()]] },
+      {
+        name = "CodeActionRange",
+        cmd = [[lua require("jdtls").code_action(true)]],
+      },
+      {
+        name = "JdtCodeActionRefactor",
+        cmd = [[lua require("jdtls").code_action(false, "refactor")]],
+      },
+      {
+        name = "OrganizeImports",
+        cmd = [[lua require("jdtls").organize_imports()]],
+      },
+      {
+        name = "ExtractVariable",
+        cmd = [[lua require("jdtls").extract_variable()]],
+      },
+      {
+        name = "ExtractVariableRange",
+        cmd = [[lua require("jdtls").extract_variable(true)]],
+      },
+      {
+        name = "ExtractConstant",
+        cmd = [[lua require("jdtls").extract_constant()]],
+      },
+      {
+        name = "ExtractConstantRange",
+        cmd = [[lua require("jdtls").extract_constant(true)]],
+      },
+      {
+        name = "ExtractMethod",
+        cmd = [[lua require("jdtls").extract_method(true)]],
+      },
+      { name = "TestClass", cmd = [[lua require("jdtls").test_class(true)]] },
+      {
+        name = "TestNearestMethod",
+        cmd = [[lua require("jdtls").test_nearest_method(true)]],
+      },
+    },
   })
 end
 
