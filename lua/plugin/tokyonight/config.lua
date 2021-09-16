6bf9209ae8ce30a870113aff98d78bff639c1f61
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-16 11:07:10 +0800

    [ plugin/tokyonight ] Add `LspDiagnosticsSign` groups
    tokyonight does not set this, instead use the `LspDiagnosticsDefault`
    groups, which is by default link by others `LspDiagnostics` related
    groups.
    
    I add this so that I can set `gui` style (such as BOLD, ITALIC, etc.) to
    the `CursorLineNr`.

--- lua/plugin/tokyonight/config.lua
+++ lua/plugin/tokyonight/config.lua
@@ -24,6 +24,10 @@ local hlgroups = {
   TabLine = ("guifg=%s guibg=%s"):format(c.fg_gutter, c.bg_statusline),
   TabLineFill = ("guifg=%s guibg=%s"):format(c.fg_gutter, c.bg_statusline),
   VertSplit = ("guifg=%s"):format(c.bg_statusline),
+  LspDiagnosticsSignError = ("gui=BOLD guifg=%s"):format(c.error),
+  LspDiagnosticsSignWarning = ("gui=BOLD guifg=%s"):format(c.warning),
+  LspDiagnosticsSignInformation = ("gui=BOLD guifg=%s"):format(c.info),
+  LspDiagnosticsSignHint = ("gui=BOLD guifg=%s"):format(c.hint),
 }
 
 for group, hl in pairs(hlgroups) do
