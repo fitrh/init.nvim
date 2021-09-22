9a90dc66bfd5602393a37d4e015e1a717fe44c03
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-22 23:58:32 +0800

    [ lsp/command ] Use `group` for `LspFormat...` user command

--- lua/lsp/command.lua
+++ lua/lsp/command.lua
@@ -1,23 +1,17 @@
-local command = require("lib.command")
-
 local M = {}
 
 function M.attach(client, bufnr)
+  local command = require("lib.command")
+
   if client.resolved_capabilities.document_formatting then
-    command.create({
+    command.group({
+      prefix = "LspFormat",
       option = "-buffer",
-      name = "LspFormat",
-      cmd = "lua vim.lsp.buf.formatting()",
-    })
-    command.create({
-      option = "-buffer",
-      name = "LspFormatSeq",
-      cmd = "lua vim.lsp.buf.formatting_seq_sync()",
-    })
-    command.create({
-      option = "-buffer",
-      name = "LspFormatSync",
-      cmd = "lua vim.lsp.buf.formatting_sync()",
+      create = {
+        { name = "", cmd = "lua vim.lsp.buf.formatting()" },
+        { name = "Seq", cmd = "lua vim.lsp.buf.formatting_seq_sync()" },
+        { name = "Sync", cmd = "lua vim.lsp.buf.formatting_sync()" },
+      },
     })
   end
 end
