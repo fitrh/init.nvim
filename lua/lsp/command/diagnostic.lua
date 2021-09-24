05c3aa6a26e40c01ae88858ee53936b324085756
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-24 11:33:14 +0800

    [ lsp/command/diagnostic ] Simplify option passing
    In `vim.diagnostic`, `show_line_diagnostics`, `goto_next`, and
    `goto_prev` have similiar option to pass to show diagnostic with rounded
    border and source name, this commit simplify that by group them together
    into a table.

--- lua/lsp/command/diagnostic.lua
+++ lua/lsp/command/diagnostic.lua
@@ -1,26 +1,21 @@
 local M = {}
 
 function M.attach()
+  local opts = '{ focusable = false, border = "rounded", source = "always" }'
+  local diagnostic = {
+    SHOW = ("lua vim.diagnostic.show_line_diagnostics(%s)"):format(opts),
+    NEXT = ("lua vim.diagnostic.goto_next({ popup_opts = %s })"):format(opts),
+    PREV = ("lua vim.diagnostic.goto_prev({ popup_opts = %s })"):format(opts),
+  }
+
   require("lib.command").group({
     prefix = "Diagnostic",
     option = "-buffer",
     create = {
-      {
-        name = "ShowInline",
-        cmd = "lua vim.diagnostic.show_line_diagnostics({ focusable = false, border = 'rounded', source = 'always'})",
-      },
-      {
-        name = "GoToNext",
-        cmd = "lua vim.diagnostic.goto_next({ popup_opts = { focusable = false, border = 'rounded', source = 'always' } })",
-      },
-      {
-        name = "GoToPrev",
-        cmd = "lua vim.diagnostic.goto_prev({ popup_opts = { focusable = false, border = 'rounded', source = 'always' } })",
-      },
-      {
-        name = "LocList",
-        cmd = "lua vim.diagnostic.setloclist()",
-      },
+      { name = "ShowInline", cmd = diagnostic.SHOW },
+      { name = "GoToNext", cmd = diagnostic.NEXT },
+      { name = "GoToPrev", cmd = diagnostic.PREV },
+      { name = "LocList", cmd = "lua vim.diagnostic.setloclist()" },
     },
   })
 end
