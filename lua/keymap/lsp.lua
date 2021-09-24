c3d6719f885ea85be0cc5486df41e1c12df024e6
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-24 11:24:03 +0800

    [ keymap/lsp ] Fix `has` function
    `vim.fn.exists` return 0 if no such command exists, to ensure the
    command exists, the `has` function should check if not 0.

--- lua/keymap/lsp.lua
+++ lua/keymap/lsp.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+local function has(cmd)
+  return vim.fn.exists(cmd) ~= 0
+end
+
 function M.attach(client, bufnr)
   local keymap = require("lib.keymap")
   local on = keymap.on_press
@@ -7,7 +11,7 @@ function M.attach(client, bufnr)
   local opt = keymap.opt
   local maps = {}
 
-  if client.resolved_capabilities.document_formatting then
+  if has(":LspFormatSync") then
     maps["Format"] = lead("<CR>", "n"):exec("LspFormatSync<CR><BAR>:w")
   end
 
@@ -41,7 +45,7 @@ function M.attach(client, bufnr)
   }
 
   for _, v in ipairs(capabilityKeys) do
-    if vim.fn.exists((":Lsp%s"):format(v.cmd)) then
+    if has((":Lsp%s"):format(v.cmd)) then
       maps[v.cmd] = on(v.key, v.mode):exec(("%s%s"):format("Lsp", v.cmd))
     end
   end
