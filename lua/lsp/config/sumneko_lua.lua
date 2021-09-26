7c8673e6f1dfdcc1259198e0191fbc07e8785c93
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-26 16:08:04 +0800

    [ lsp/config/sumneko_lua ] Specifies `lua/` as `workspace.library`
    Load all directories from runtime path causing heavy load for the LSP.

--- lua/lsp/config/sumneko_lua.lua
+++ lua/lsp/config/sumneko_lua.lua
@@ -21,7 +21,7 @@ return {
       },
       diagnostics = { globals = { "vim" } },
       workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = vim.api.nvim_get_runtime_file("lua", true),
       },
       telemetery = { enable = false },
     },
