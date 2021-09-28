7127fbef569ee498b1cbfae62ef372050b07afbc
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-28 11:23:32 +0800

    [ plugin/lspsignature ] Update `hint_prefix`

--- lua/plugin/lspsignature/config.lua
+++ lua/plugin/lspsignature/config.lua
@@ -1,7 +1,7 @@
 local M = {
   bind = true,
   floating_window = false,
-  hint_prefix = " ",
+  hint_prefix = " () ",
   hint_scheme = "LspDiagnosticsDefaultHint",
   hi_parameter = "LspSignatureActiveParameter",
   extra_trigger_chars = { "(", "{", "," },
