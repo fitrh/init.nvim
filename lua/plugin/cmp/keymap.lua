ea44de25f6c53c00100987c1942555a4215ade6f
fitrh <fitrahm17h@student.unhas.ac.id>
2021-10-09 17:19:53 +0800

    [ plugin/cmp ] Update keymap configuration, upstream follow up
    ref: https://github.com/hrsh7th/nvim-cmp/pull/224

--- lua/plugin/cmp/keymap.lua
+++ lua/plugin/cmp/keymap.lua
@@ -11,8 +11,6 @@ end
 
 function M.setup(cmp)
   local snippet = require("luasnip")
-  local termcode = require("lib.keymap").termcode
-  local feedkeys = vim.api.nvim_feedkeys
 
   return {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -26,8 +24,8 @@ function M.setup(cmp)
       select = true,
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkeys(termcode("<C-n>"), "n", true)
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif snippet.expand_or_jumpable() then
         snippet.expand_or_jump()
       elseif has_words_before() then
@@ -40,8 +38,8 @@ function M.setup(cmp)
       "s",
     }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkeys(termcode("<C-p>"), "n", true)
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif snippet.jumpable(-1) then
         snippet.jump(-1)
       else
