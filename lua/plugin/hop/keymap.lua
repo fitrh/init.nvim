d8b86f49c1a559ff9bddc470f9405e47e9e394c0
fitrh <fitrahm17h@student.unhas.ac.id>
2021-10-09 14:41:59 +0800

    [ plugin/hop ] Update keymap, 166d154 follow up

--- lua/plugin/hop/keymap.lua
+++ lua/plugin/hop/keymap.lua
@@ -1,12 +1,13 @@
 local keymap = require("lib.keymap")
 local on = keymap.on_press
-local lead = keymap.on_press_leader
 local opt = keymap.opt
 
 keymap.bind({
-  lead("j"):exec("HopWord"),
-  on("f"):exec("HopChar2"),
-  on("F"):exec("HopLineStart"),
+  on("ff"):exec("HopWord"),
+  on("fj"):exec("HopChar2AC"),
+  on("fk"):exec("HopChar2BC"),
+  on("Fj"):exec("HopWordAC"),
+  on("Fk"):exec("HopWordBC"),
   on("<M-f>"):exec("HopPattern"),
 }, {
   options = opt():noremap(),
