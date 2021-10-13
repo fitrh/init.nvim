8e7f61294c173f8446399e22cc4fb0b3604a1fa1
fitrh <fitrahm17h@student.unhas.ac.id>
2021-10-13 22:45:57 +0800

    [ plugin/kommentary ] Add keymap for increase/decrease comment level

--- lua/plugin/kommentary/keymap.lua
+++ lua/plugin/kommentary/keymap.lua
@@ -5,4 +5,10 @@ keymap.bind({
   lead("/", "n"):plug("kommentary_line_default"),
   lead("/", "v"):plug("kommentary_visual_default"),
   lead("c", "n"):plug("kommentary_motion_default"),
+  lead("cic", "n"):plug("kommentary_line_increase"),
+  lead("ci", "n"):plug("kommentary_motion_increase"),
+  lead("ci", "x"):plug("kommentary_visual_increase"),
+  lead("cdc", "n"):plug("kommentary_line_decrease"),
+  lead("cd", "n"):plug("kommentary_motion_decrease"),
+  lead("cd", "x"):plug("kommentary_visual_decrease"),
 })
