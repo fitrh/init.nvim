8e23052024090b6f1197a6d43483025ea32caef1
fitrh <fitrahm17h@student.unhas.ac.id>
2021-11-14 11:29:07 +0800

    [plugin/hop] Add `..CurrentLine..` commands and set the keymaps

--- lua/plugin/hop/keymap.lua
+++ lua/plugin/hop/keymap.lua
@@ -6,8 +6,12 @@ keymap.bind({
   on("ff"):exec("HopWord"),
   on("fj"):exec("HopChar2AC"),
   on("fk"):exec("HopChar2BC"),
+  on("fl"):exec("HopChar2CurrentLineAC"),
+  on("fh"):exec("HopChar2CurrentLineBC"),
   on("Fj"):exec("HopWordAC"),
   on("Fk"):exec("HopWordBC"),
+  on("Fl"):exec("HopWordCurrentLineAC"),
+  on("Fh"):exec("HopWordCurrentLineBC"),
   on("<M-f>"):exec("HopPattern"),
 }, {
   options = opt():noremap(),
