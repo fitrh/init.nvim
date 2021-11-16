05337d2831ed9dcefa13a9a6ffcaa1434b6d7b0e
fitrh <fitrahm17h@student.unhas.ac.id>
2021-11-16 22:17:51 +0800

    [lsp/config/tsserver] Add `InlayHints` support
    ref: https://github.com/jose-elias-alvarez/nvim-lsp-ts-utils/pull/79

--- lua/lsp/config/tsserver.lua
+++ lua/lsp/config/tsserver.lua
@@ -2,10 +2,18 @@ return {
   capabilities = require("lsp.capability"),
   handlers = require("lsp.handler").default(),
   init_options = {
+    hostInfo = "neovim",
     preferences = {
       quotePreference = "double",
       includeCompletionsWithSnippetText = true,
       generateReturnInDocTemplate = true,
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
     },
   },
   on_attach = function(client, bufnr)
