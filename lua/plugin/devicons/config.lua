aaa56c1a36d9f027f931bc87e3fa0bce3384e95c
fitrh <fitrahm17h@student.unhas.ac.id>
2021-11-18 18:47:24 +0800

    [plugin/devicons] Extract `nvim-web-devicons` into its own directory
    Also, set `default` to true and override some icons.

---
+++ lua/plugin/devicons/config.lua
@@ -0,0 +1,35 @@
+require("nvim-web-devicons").setup({
+  override = {
+    javascript = {
+      icon = "",
+      color = "#8fa959",
+      name = "JavaScript",
+    },
+    log = {
+      icon = "",
+      color = "#41535b",
+      name = "Log",
+    },
+    make = {
+      icon = "",
+      color = "#e37933",
+      name = "Makefile",
+    },
+    ["package.json"] = {
+      icon = "",
+      color = "#8fa959",
+      name = "JavaScript",
+    },
+    python = {
+      icon = "",
+      color = "#3572A5",
+      name = "Python",
+    },
+    txt = {
+      icon = "",
+      color = "#6a9fb5",
+      name = "TXT",
+    },
+  },
+  default = true,
+})
