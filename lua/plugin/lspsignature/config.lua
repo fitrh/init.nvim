99cf5b2457933673c8ee0cab50900006e5c610b9
fitrh <fitrahm17h@student.unhas.ac.id>
2021-11-22 18:10:37 +0800

    [plugin/lspsignature] Set some floating window configuration
    - Set max height and width
    - Set transparency, following the `pumblend` option
    - Set toggle key in insert mode

--- lua/plugin/lspsignature/config.lua
+++ lua/plugin/lspsignature/config.lua
@@ -4,8 +4,12 @@ local M = {
   hint_prefix = " () ",
   hint_scheme = "DiagnosticHint",
   hi_parameter = "LspSignatureActiveParameter",
+  max_height = 10,
+  max_width = 80,
   extra_trigger_chars = { "(", "{", "," },
   zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
+  tranparency = vim.api.nvim_get_option("pumblend"),
+  toggle_key = "<M-x>",
 }
 
 return M
