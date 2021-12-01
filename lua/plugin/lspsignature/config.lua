2e9300ad84782ef52294ed96d259c3f9755c8ca2
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-01 23:13:33 +0800

    [plugin/lspsignature] Disable API DOC and enable floating window

--- lua/plugin/lspsignature/config.lua
+++ lua/plugin/lspsignature/config.lua
@@ -1,13 +1,12 @@
 local M = {
   bind = true,
-  floating_window = false,
+  doc_lines = 0, -- 0 means disable API DOC in INSERT mode
+  floating_window = true,
   hint_prefix = " () ",
   hint_scheme = "DiagnosticHint",
   hi_parameter = "LspSignatureActiveParameter",
-  max_height = 10,
-  max_width = 80,
   extra_trigger_chars = { "(", "{", "," },
-  zindex = 50, -- by default it will be on top of all floating windows, set to 50 send it to bottom
+  zindex = 200, -- 50 makes its floating windows below other floating windows
   tranparency = vim.api.nvim_get_option("pumblend"),
   toggle_key = "<M-x>",
 }
