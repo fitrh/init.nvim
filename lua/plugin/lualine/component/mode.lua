a5c918c960090e58d32d2141f107000c90eb0000
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-16 17:24:44 +0800

    [plugin.lualinen.component.mode] Add more mode

--- lua/plugin/lualine/component/mode.lua
+++ lua/plugin/lualine/component/mode.lua
@@ -6,6 +6,8 @@ return {
     local modes = {
       n = { text = "NRM", color = palette.blue },
       i = { text = "INS", color = palette.green.primary },
+      ix = { text = "I·X", color = palette.green.primary }, -- i_CTRL-N
+      ic = { text = "I·C", color = palette.green.primary }, -- i_CTRL-X
       v = { text = "VIS", color = palette.purple },
       [""] = { text = "V·B", color = palette.purple },
       V = { text = "V·L", color = palette.purple },
@@ -13,6 +15,8 @@ return {
       s = { text = "SEL", color = palette.purple },
       S = { text = "S·L", color = palette.purple },
       [""] = { text = "S·B", color = palette.purple },
+      R = { text = "REP", color = palette.red },
+      Rv = { text = "R·V", color = palette.red }, -- gR
       t = { text = "TRM", color = palette.red },
     }
     local mode = modes[vim.api.nvim_get_mode().mode]
