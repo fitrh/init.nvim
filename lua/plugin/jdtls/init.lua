a10292af9a96cdc4d9ee1a7597a5fd5adc15a153
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-20 17:55:31 +0800

    [plugin.jdtls] Create `JdtStart` user command

--- lua/plugin/jdtls/init.lua
+++ lua/plugin/jdtls/init.lua
@@ -1,12 +1,16 @@
 return {
   "mfussenegger/nvim-jdtls",
   config = function()
+    require("lib.command").create({
+      name = "JdtStart",
+      cmd = 'lua require("plugin.jdtls.config")()',
+    })
     require("lib.event.augroup").setup({
       ["AttachJDTLS"] = {
         {
           events = "FileType",
           filter = "java",
-          cmd = [[lua require("plugin.jdtls.config").attach()]],
+          cmd = "JdtStart",
         },
       },
     })
