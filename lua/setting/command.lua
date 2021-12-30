60381c304907d5a3e186091fbcc569265707a0e6
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-30 18:37:46 +0800

    [setting.command] Adapt to 50173b1

--- lua/setting/command.lua
+++ lua/setting/command.lua
@@ -1,27 +1,49 @@
 local command = require("lib.command")
 
 -- dir helper
-command.create({ name = "MakeDir", cmd = [[lua require("helper.dir").mk()]] })
+command.add("MakeDir", require("helper.dir").mk)
 
 -- tmux helper
 command.group({
   prefix = "TmuxMove",
-  create = {
-    { name = "H", cmd = [[lua require("helper.tmux").move("h")]] },
-    { name = "J", cmd = [[lua require("helper.tmux").move("j")]] },
-    { name = "K", cmd = [[lua require("helper.tmux").move("k")]] },
-    { name = "L", cmd = [[lua require("helper.tmux").move("l")]] },
+  cmds = {
+    {
+      name = "H",
+      cmd = function()
+        require("helper.tmux").move("h")
+      end,
+    },
+    {
+      name = "J",
+      cmd = function()
+        require("helper.tmux").move("j")
+      end,
+    },
+    {
+      name = "K",
+      cmd = function()
+        require("helper.tmux").move("k")
+      end,
+    },
+    {
+      name = "L",
+      cmd = function()
+        require("helper.tmux").move("l")
+      end,
+    },
   },
 })
 
 -- zen helper
 command.group({
   prefix = "ZenToggle",
-  create = {
-    { cmd = [[lua require("helper.zen").toggle()]] },
+  cmds = {
+    { cmd = require("helper.zen").toggle },
     {
       name = "Full",
-      cmd = [[lua require("helper.zen").toggle({ laststatus = true, })]],
+      cmd = function()
+        require("helper.zen").toggle({ laststatus = true })
+      end,
     },
   },
 })
