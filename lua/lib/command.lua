50173b1721f66a033b291de58c477e33e17ab09e
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-30 18:31:43 +0800

    [lib.command] Use lua API to define user commands
    ref: https://github.com/neovim/neovim/pull/16752

--- lua/lib/command.lua
+++ lua/lib/command.lua
@@ -1,30 +1,29 @@
-local str = require("helper.str")
-
 local M = {}
 
-function M.create(args)
-  local opts = args or {}
-  local option = opts.options or "-nargs=0"
-  local name = str.ufirst(opts.name)
-  local cmd = opts.cmd
-  cmd = ("command! %s %s %s"):format(option, name, cmd)
-  vim.cmd(cmd)
+function M.add(name, cmd, opts)
+  opts = opts or {}
+  local options = opts.opts or { nargs = 0, force = true }
+  local buf = opts.buf or false
+
+  if buf then
+    local nr = type(buf) == "number" and buf or 0
+    vim.api.nvim_buf_add_user_command(nr, name, cmd, options)
+  else
+    vim.api.nvim_add_user_command(name, cmd, options)
+  end
 end
 
 function M.group(args)
   local opts = args or {}
   local prefix = opts.prefix or nil
-  local option = opts.option or nil
-  local create = opts.create or {}
-  for _, v in ipairs(create) do
-    local name = prefix
-        and ("%s%s"):format(
-          str.ufirst(prefix),
-          str.ufirst(v.name or "")
-        )
-      or str.ufirst(v.name)
-    option = option or v.option
-    M.create({ option = option, name = name, cmd = v.cmd })
+  local options = opts.opts or nil
+  local buf = opts.buf or nil
+  local cmds = opts.cmds or {}
+  for _, v in ipairs(cmds) do
+    local name = prefix and ("%s%s"):format(prefix, v.name or "") or v.name
+    options = options or v.opts
+    buf = buf or v.buf
+    M.add(name, v.cmd, { options = options, buf = buf })
   end
 end
 
