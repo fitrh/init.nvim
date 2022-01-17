69b599324146450ff75d24ee22a5c9dd4112e036
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-17 18:43:24 +0800

    [lsp.config.clangd] Remove obsolete `--backround-index` flag

--- lua/lsp/config/clangd.lua
+++ lua/lsp/config/clangd.lua
@@ -7,7 +7,6 @@ return {
   cmd = {
     -- see clangd --help-hidden
     "clangd",
-    "--background-index",
     -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
     -- to add more checks, create .clang-tidy file in the root directory
     -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
