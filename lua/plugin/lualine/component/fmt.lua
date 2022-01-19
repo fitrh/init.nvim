6d4a5f959394c2260e10cb8da1405c2003cdd285
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-19 23:56:09 +0800

    [plugin.lualine.component] Add `fmt`, component formatter

---
+++ lua/plugin/lualine/component/fmt.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+---Format output to show `str` if window width equal or larger than `width`,
+---otherwise, show `fallback`
+---
+---@param width number #expected width to show the string
+---@param str string #string to show
+---@param fallback string #string to show if window width less than `width`
+---@return string
+function M.show_on_width(width, str, fallback)
+  local win_width = vim.api.nvim_win_get_width(0) - #str
+  if win_width > (width - 1) then
+    return str
+  end
+  return fallback
+end
+
+return M
