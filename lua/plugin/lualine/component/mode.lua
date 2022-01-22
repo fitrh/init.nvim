5d6bba140876b7f06ba1db3a1f9f4671e629423d
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-22 19:34:45 +0800

    [plugin.lualine.component.mode] Move `modes` table outside function
    Prevents table creation on every function call.

--- lua/plugin/lualine/component/mode.lua
+++ lua/plugin/lualine/component/mode.lua
@@ -2,24 +2,25 @@ local palette = require("plugin.themes.palette")
 local highlight = require("lib.highlight")
 local fmt = require("plugin.lualine.component.fmt")
 
+local modes = {
+  n = { text = "NRM", color = palette.blue },
+  i = { text = "INS", color = palette.green.primary },
+  ix = { text = "I·X", color = palette.green.primary }, -- i_CTRL-N
+  ic = { text = "I·C", color = palette.green.primary }, -- i_CTRL-X
+  v = { text = "VIS", color = palette.purple },
+  [""] = { text = "V·B", color = palette.purple },
+  V = { text = "V·L", color = palette.purple },
+  c = { text = "CMD", color = palette.yellow },
+  s = { text = "SEL", color = palette.purple },
+  S = { text = "S·L", color = palette.purple },
+  [""] = { text = "S·B", color = palette.purple },
+  R = { text = "REP", color = palette.red },
+  Rv = { text = "R·V", color = palette.red }, -- gR
+  t = { text = "TRM", color = palette.red },
+}
+
 return {
   function()
-    local modes = {
-      n = { text = "NRM", color = palette.blue },
-      i = { text = "INS", color = palette.green.primary },
-      ix = { text = "I·X", color = palette.green.primary }, -- i_CTRL-N
-      ic = { text = "I·C", color = palette.green.primary }, -- i_CTRL-X
-      v = { text = "VIS", color = palette.purple },
-      [""] = { text = "V·B", color = palette.purple },
-      V = { text = "V·L", color = palette.purple },
-      c = { text = "CMD", color = palette.yellow },
-      s = { text = "SEL", color = palette.purple },
-      S = { text = "S·L", color = palette.purple },
-      [""] = { text = "S·B", color = palette.purple },
-      R = { text = "REP", color = palette.red },
-      Rv = { text = "R·V", color = palette.red }, -- gR
-      t = { text = "TRM", color = palette.red },
-    }
     local mode = modes[vim.api.nvim_get_mode().mode]
     highlight.set("LualineComponentMode", {
       fg = mode.color,
