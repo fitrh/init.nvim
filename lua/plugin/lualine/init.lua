0878b10c8add1b6086cfe424efabca8e78a233a1
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-26 20:54:27 +0800

    [plugin.lualine] Add `lsp-progress` plugin

--- lua/plugin/lualine/init.lua
+++ lua/plugin/lualine/init.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-lualine/lualine.nvim",
-  requires = { "kyazdani42/nvim-web-devicons" },
+  requires = {
+    { "kyazdani42/nvim-web-devicons" },
+    { "arkav/lualine-lsp-progress", module = "lualine" },
+  },
   config = function()
     require("plugin.lualine.config")
   end,
