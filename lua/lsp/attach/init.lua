5ba8f5942f7eaa022ecf964b50dba9fb4f4974dd
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-26 19:57:45 +0800

    [plugin] Remove the remaining `lsp-status.nvim` references

--- lua/lsp/attach/init.lua
+++ lua/lsp/attach/init.lua
@@ -3,7 +3,6 @@ local M = {}
 M.with = {
   default = require("lsp.attach.with_default"),
   lsp = {
-    status = require("lsp.attach.with_lsp_status"),
     signature = require("lsp.attach.with_lsp_signature"),
   },
   all = require("lsp.attach.with_all_extension"),
