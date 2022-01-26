9fc7279b77d76e64a31bcb0967f9daabef65bf5d
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-26 21:12:10 +0800

    [plugin.lualine] Fix module name for `lsp-progress`

--- lua/plugin/lualine/init.lua
+++ lua/plugin/lualine/init.lua
@@ -2,7 +2,10 @@ return {
   "nvim-lualine/lualine.nvim",
   requires = {
     { "kyazdani42/nvim-web-devicons" },
-    { "arkav/lualine-lsp-progress", module = "lualine" },
+    {
+      "arkav/lualine-lsp-progress",
+      module = "lualine.components.lsp_progress",
+    },
   },
   config = function()
     require("plugin.lualine.config")
