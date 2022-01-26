20a7ea6307f9e8b64bf1082899bd391d04a23182
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-26 20:54:55 +0800

    [plugin.lualine.component.lsp] REFACTOR! use `lsp-progress` plugin

--- lua/plugin/lualine/component/lsp.lua
+++ lua/plugin/lualine/component/lsp.lua
@@ -1,41 +1,17 @@
 local M = {}
+local palette = require("plugin.themes.palette")
 
-function M.setup(arg)
-  local opts = arg or {}
-  local foreground = opts.fg or "blue"
-  local lsp = require("lsp-status")
-  return {
-    server = {
-      function()
-        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
-        if not next(clients) then
-          return nil
-        end
-        -- FIXME: avoid loop for get the active client (?)
-        for _, client in ipairs(clients) do
-          local filetypes = client.config.filetypes
-          if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-            return client.name
-          end
-        end
-        return nil
-      end,
-      color = { fg = foreground, gui = "BOLD" },
-    },
-    status = {
-      function()
-        return lsp.status()
-      end,
-      cond = function()
-        local width = vim.api.nvim_win_get_width(0) - #lsp.status()
-        local client_attached = #vim.lsp.buf_get_clients() > 0
-        local status_not_empty = lsp.status() ~= " "
-        return width > 99 and client_attached and status_not_empty
-      end,
-      color = { fg = foreground, gui = "BOLD" },
-    },
-  }
-end
+M.progress = {
+  "lsp_progress",
+  fmt = function(str)
+    local width = vim.api.nvim_win_get_width(0) - #str
+    if width < 100 and #str > 16 then
+      return ("...%s"):format(str:sub(#str - 16, #str))
+    end
+    return str
+  end,
+  color = { fg = palette.blue2, gui = "BOLD" },
+  spinner_symbols = { " ", " ", " ", " ", " ", " " },
+}
 
 return M
