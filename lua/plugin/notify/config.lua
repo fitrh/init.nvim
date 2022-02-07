f2f787939bc3c9f92d7bf8271658b4a0ab28f20d
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-07 21:05:56 +0800

    [plugin.notify] Set `max_{width|height}`

--- lua/plugin/notify/config.lua
+++ lua/plugin/notify/config.lua
@@ -2,6 +2,8 @@ local notify = require("notify")
 local sign = require("helper.sign").diagnostic
 local config = {}
 
+config.max_width = math.floor(vim.api.nvim_win_get_width(0) / 3)
+config.max_height = math.floor(vim.api.nvim_win_get_height(0) / 3)
 config.timeout = 3000
 config.icons = {
   ERROR = sign.ERROR,
