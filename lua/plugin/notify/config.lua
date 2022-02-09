b024d4795763c18b29557da98013ab7a88917298
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-09 18:08:40 +0800

    [plugin.notify] Use function for `max_{width,height}` get dynamic value

--- lua/plugin/notify/config.lua
+++ lua/plugin/notify/config.lua
@@ -2,8 +2,14 @@ local notify = require("notify")
 local sign = require("helper.sign").diagnostic
 local config = {}
 
-config.max_width = math.floor(vim.api.nvim_win_get_width(0) / 3)
-config.max_height = math.floor(vim.api.nvim_win_get_height(0) / 3)
+config.max_width = function()
+  return math.floor(math.max(vim.api.nvim_get_option("columns") / 3, 20))
+end
+
+config.max_height = function()
+  return math.floor(math.max(vim.api.nvim_get_option("lines") / 3, 4))
+end
+
 config.timeout = 3000
 config.icons = {
   ERROR = sign.ERROR,
