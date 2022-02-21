afe1518843ee2e02d94044f4a2bbfd174fdd4356
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-21 18:55:25 +0800

    [plugin.lualine.component.util.alignment] Allow pass string as component

--- lua/plugin/lualine/component/util/alignment.lua
+++ lua/plugin/lualine/component/util/alignment.lua
@@ -1,7 +1,10 @@
----@param component table
+---@param component string|table
 ---@param padding table
 ---@return table component
 local function alignment(component, padding)
+  if type(component) == "string" then
+    component = { component }
+  end
   component.padding = padding
   return component
 end
@@ -9,21 +12,21 @@ end
 local Align = {}
 
 ---component.padding = { left = 0, right = 1 }
----@param component table
+---@param component string|table
 ---@return table component
 function Align.left(component)
   return alignment(component, { left = 0, right = 1 })
 end
 
 ---component.padding = { left = 1, right = 0 }
----@param component table
+---@param component string|table
 ---@return table component
 function Align.right(component)
   return alignment(component, { left = 1, right = 0 })
 end
 
 ---component.padding = 0
----@param component table
+---@param component string|table
 ---@return table component
 function Align.no(component)
   return alignment(component, 0)
