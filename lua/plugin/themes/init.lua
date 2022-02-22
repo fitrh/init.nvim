001dbf280dd9d503605908daa34516d43e44f53a
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-22 11:56:08 +0800

    [plugin.themes] Replace symlink with files
    This way, we can put logic to load colorscheme base on `NVIM_COLORSCHME`
    environmenr variable.

--- lua/plugin/themes/init.lua
+++ lua/plugin/themes/init.lua
@@ -1,1 +1,5 @@
-tokyonight/init.lua
+local THEME = os.getenv("NVIM_COLORSCHEME") or "tokyonight"
+local module = ("plugin.themes.%s"):format(THEME)
+local ok, result = pcall(require, module)
+
+return ok and result
--- lua/plugin/themes/palette.lua
+++ lua/plugin/themes/palette.lua
@@ -1,1 +1,5 @@
-tokyonight/palette.lua
+local THEME = os.getenv("NVIM_COLORSCHEME") or "tokyonight"
+local module = ("plugin.themes.%s.palette"):format(THEME)
+local ok, result = pcall(require, module)
+
+return ok and result or nil
