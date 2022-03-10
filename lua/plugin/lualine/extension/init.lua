bb99299bdc86cc8f2bef8cb10c508478fc98064c
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-10 18:11:19 +0800

    [plugin.lualine.extension] Add extension for `trouble.nvim`

--- lua/plugin/lualine/extension/init.lua
+++ lua/plugin/lualine/extension/init.lua
@@ -1,4 +1,5 @@
 return {
   help = require("plugin.lualine.extension.help"),
-  terminal = require("plugin.lualine.extension.terminal")
+  terminal = require("plugin.lualine.extension.terminal"),
+  trouble = require("plugin.lualine.extension.trouble"),
 }
