eb794aad65b80806cc325f593c196dfe34aee42e
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-11 17:39:31 +0800

    [plugin.kommentary] Move keymap init into the init file
    Plugin initialization settings should be responsible for registering
    their keymap.

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -1,3 +1,2 @@
 require("keymap.nvim")
-require("plugin.kommentary.keymap")
 require("keymap.user")
--- lua/plugin/kommentary/init.lua
+++ lua/plugin/kommentary/init.lua
@@ -2,6 +2,7 @@ return {
   "b3nj5m1n/kommentary",
   config = function()
     require("plugin.kommentary.config")
+    require("plugin.kommentary.keymap")
   end,
   setup = function()
     vim.g.kommentary_create_default_mappings = false
@@ -10,5 +11,10 @@ return {
     "<Plug>kommentary_line_default",
     "<Plug>kommentary_visual_default",
     "<Plug>kommentary_motion_default",
+    { "n", "<Leader>/" },
+    { "v", "<Leader>/" },
+    { "n", "<Leader>c" },
+    { "v", "<Leader>ci" },
+    { "v", "<Leader>cd" },
   },
 }
