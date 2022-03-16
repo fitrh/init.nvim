a4c93fe91a6762523ce31c1308aef71ed221d83b
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-16 23:06:27 +0800

    [after/ftplugin/python] Refactor to use `vim.keymap.set` API
    This commit:
    
    - Use `vim.opt_local` to set options
    - Use `pcall` to check if a module exist
    - Use `vim.keymap.set` API to set keymap

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,9 +1,9 @@
-vim.api.nvim_buf_set_option(0, "textwidth", 79)
-if vim.fn.exists(":Telescope") ~= 0 then
-  vim.api.nvim_set_keymap(
-    "n",
-    "<Leader>bb",
-    "<Cmd>Telescope buffers<CR>",
-    { noremap = true, silent = true }
-  )
+local set = vim.opt_local
+
+set.textwidth = 79
+
+local telescope = {}
+telescope.ok, telescope.module = pcall(require, "telescope.builtin")
+if telescope.ok then
+  vim.keymap.set("n", "gb", telescope.module.buffers, { buffer = 0 })
 end
