11a81c01f01b991153a43c232b80b8c9c5d092ce
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-17 18:43:22 +0800

    [plugin.themes.tokyonight] Use `lib.highlight`

--- lua/plugin/themes/tokyonight/config.lua
+++ lua/plugin/themes/tokyonight/config.lua
@@ -17,22 +17,24 @@ g.tokyonight_sidebars = config.sidebars
 vim.cmd("colorscheme tokyonight")
 
 local c = require("tokyonight.colors").setup(config)
+local hi = require("lib.highlight")
+
 local hlgroups = {
-  CursorLineNr = ("gui=BOLD guifg=%s"):format(c.blue),
-  ColorColumn = ("guibg=%s"):format(c.bg_statusline),
-  TabLineSel = ("guifg=%s guibg=%s"):format(c.fg, c.bg),
-  TabLine = ("guifg=%s guibg=%s"):format(c.fg_gutter, c.bg_statusline),
-  TabLineFill = ("guifg=%s guibg=%s"):format(c.fg_gutter, c.bg_statusline),
-  DiagnosticSignError = ("gui=BOLD guifg=%s"):format(c.error),
-  DiagnosticSignWarn = ("gui=BOLD guifg=%s"):format(c.warning),
-  DiagnosticSignInfo = ("gui=BOLD guifg=%s"):format(c.info),
-  DiagnosticSignHint = ("gui=BOLD guifg=%s"):format(c.hint),
-  GitSignsAddNr = ("gui=bold guifg=%s"):format(c.gitSigns.add),
-  GitSignsChangeNr = ("gui=bold guifg=%s"):format(c.gitSigns.change),
-  GitSignsDeleteNr = ("gui=bold guifg=%s"):format(c.gitSigns.delete),
-  GitSignsDeleteLn = ("guifg=%s"):format(c.gitSigns.delete),
+  CursorLineNr = { fg = c.blue, bold = true },
+  ColorColumn = { bg = c.bg_statusline },
+  TabLineSel = { fg = c.fg, bg = c.bg },
+  TabLine = { fg = c.fg_gutter, bg = c.bg_statusline },
+  TabLineFill = { fg = c.fg_gutter, bg = c.bg_statusline },
+  DiagnosticSignError = { fg = c.error, bold = true },
+  DiagnosticSignWarn = { fg = c.warning, bold = true },
+  DiagnosticSignInfo = { fg = c.info, bold = true },
+  DiagnosticSignHint = { fg = c.hint, bold = true },
+  GitSignsAddNr = { fg = c.gitSigns.add, bold = true },
+  GitSignsChangeNr = { fg = c.gitSigns.change, bold = true },
+  GitSignsDeleteNr = { fg = c.gitSigns.delete, bold = true },
+  GitSignsDeleteLn = { fg = c.gitSigns.delete },
 }
 
 for group, hl in pairs(hlgroups) do
-  vim.cmd(("highlight %s %s"):format(group, hl))
+  hi.set(group, hl)
 end
