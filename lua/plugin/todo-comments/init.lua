90c2efceda35e1c69ad2f0ff10cf7ad59916c9cc
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-17 17:51:44 +0800

    [plugin.todo-comments] Set `requires` plugin

--- lua/plugin/todo-comments/init.lua
+++ lua/plugin/todo-comments/init.lua
@@ -1,6 +1,10 @@
 return {
   "folke/todo-comments.nvim",
   after = "nvim-treesitter",
+  requires = {
+    { "nvim-lua/plenary.nvim", module = "plenary" },
+    { "nvim-treesitter/nvim-treesitter", module = "nvim-treesitter" },
+  },
   config = function()
     require("plugin.todo-comments.config")
     require("plugin.todo-comments.keymap")
