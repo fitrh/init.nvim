023e6a1bb735f91b26b566182339564917ea5b0b
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-18 16:17:32 +0800

    [after/ftplugin] Add `TelescopePrompt.lua`, and remove telescope event

--- lua/plugin/telescope/event.lua
+++
@@ -1,9 +0,0 @@
-local augroup = require("lib.augroup")
-
-augroup("OnTelescopePrompt", {
-  "FileType",
-  pattern = "TelescopePrompt",
-  callback = function()
-    vim.api.nvim_win_set_option(0, "cursorline", false)
-  end,
-})
