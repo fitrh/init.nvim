0dce66117a9d47625ec967cad3b3206230762363
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-25 02:19:02 +0800

    [setting.option] Rename `opt` to `set`

--- lua/setting/option.lua
+++ lua/setting/option.lua
@@ -1,17 +1,17 @@
-local opt = vim.opt
+local set = vim.opt
 
-opt.backup = false -- but do not persist backup after successful write
-opt.backupcopy = "auto" -- best option for backup strategy
-opt.breakindent = true
-opt.colorcolumn = { "+1" } -- draw a vertical ruler at (textwidth + 1)th column
-opt.complete:remove({ "u", "t" })
-opt.completeopt:append({ "menuone", "noinsert" })
-opt.conceallevel = 2 -- hide concealed text unless it has a custom replacement
-opt.confirm = true -- raise a dialogue asking if you wish to save changed files
-opt.cursorline = true -- highlight current line
-opt.equalalways = true -- avoid resizing the main window when open a split
-opt.expandtab = true -- by default, always use space!
-opt.fillchars = {
+set.backup = false -- but do not persist backup after successful write
+set.backupcopy = "auto" -- best option for backup strategy
+set.breakindent = true
+set.colorcolumn = { "+1" } -- draw a vertical ruler at (textwidth + 1)th column
+set.complete:remove({ "u", "t" })
+set.completeopt:append({ "menuone", "noinsert" })
+set.conceallevel = 2 -- hide concealed text unless it has a custom replacement
+set.confirm = true -- raise a dialogue asking if you wish to save changed files
+set.cursorline = true -- highlight current line
+set.equalalways = true -- avoid resizing the main window when open a split
+set.expandtab = true -- by default, always use space!
+set.fillchars = {
   horiz = "━",
   horizup = "┻",
   horizdown = "┳",
@@ -21,49 +21,49 @@ opt.fillchars = {
   verthoriz = "╋",
   eob = " ", -- use 'space' for lines after the last buffer line in a window
 }
-opt.formatoptions:append({ "n" })
-opt.formatoptions:remove({ "t", "o", "2" })
-opt.guicursor = "n-c:hor50,i-ci-ve:ver25"
-opt.hidden = true -- enable switching unsaved buffer
-opt.ignorecase = true
-opt.laststatus = 0 -- disable statusline
-opt.nrformats:append({ "alpha" }) -- increment/decrement for alphabet
-opt.number = true
-opt.numberwidth = 1
-opt.pastetoggle = "<F2>" -- toggling paste mode when press this key
-opt.path:append({ "**" })
-opt.pumblend = 10 -- pseudo-transparency for the popup-menu, value : 0 - 100
-opt.pumheight = 10 -- maximum number of items to show in the pop-up menu
-opt.relativenumber = true
-opt.ruler = false -- hide line & column information, use g Ctrl + g instead
-opt.scrolloff = 10 -- minimal number of line to keep around the cursor
-opt.shell = "/usr/bin/bash"
-opt.shiftwidth = 4
-opt.shortmess = {
+set.formatoptions:append({ "n" })
+set.formatoptions:remove({ "t", "o", "2" })
+set.guicursor = "n-c:hor50,i-ci-ve:ver25"
+set.hidden = true -- enable switching unsaved buffer
+set.ignorecase = true
+set.laststatus = 0 -- disable statusline
+set.nrformats:append({ "alpha" }) -- increment/decrement for alphabet
+set.number = true
+set.numberwidth = 1
+set.pastetoggle = "<F2>" -- toggling paste mode when press this key
+set.path:append({ "**" })
+set.pumblend = 10 -- pseudo-transparency for the popup-menu, value : 0 - 100
+set.pumheight = 10 -- maximum number of items to show in the pop-up menu
+set.relativenumber = true
+set.ruler = false -- hide line & column information, use g Ctrl + g instead
+set.scrolloff = 10 -- minimal number of line to keep around the cursor
+set.shell = "/usr/bin/bash"
+set.shiftwidth = 4
+set.shortmess = {
   a = true,
   t = true,
   T = true,
   c = true,
   F = true,
 }
-opt.showcmd = false
-opt.showmatch = true -- highlight matching <[{()}]>
-opt.showmode = false
-opt.showtabline = 1 -- show tab-page only if there are at least 2 tab pages
-opt.sidescrolloff = 10 -- minimal number of column to keep around the cursor
-opt.signcolumn = "yes"
-opt.smartcase = true
-opt.softtabstop = 4
-opt.splitbelow = true
-opt.splitright = true
-opt.synmaxcol = 120 -- do syntax highlighting until synmaxcol character
-opt.termguicolors = true -- enable 24-bit RGB color in the TUI
-opt.textwidth = 80 -- set 80 character as default line length
-opt.undofile = true -- save undo history to an undo file on BufWrite
-opt.undolevels = 100 -- maximum number of changes than can be undone
-opt.updatetime = 250 -- if this many ms nothing is typed, write the swap file
-opt.visualbell = true
-opt.wildignore = {
+set.showcmd = false
+set.showmatch = true -- highlight matching <[{()}]>
+set.showmode = false
+set.showtabline = 1 -- show tab-page only if there are at least 2 tab pages
+set.sidescrolloff = 10 -- minimal number of column to keep around the cursor
+set.signcolumn = "yes"
+set.smartcase = true
+set.softtabstop = 4
+set.splitbelow = true
+set.splitright = true
+set.synmaxcol = 120 -- do syntax highlighting until synmaxcol character
+set.termguicolors = true -- enable 24-bit RGB color in the TUI
+set.textwidth = 80 -- set 80 character as default line length
+set.undofile = true -- save undo history to an undo file on BufWrite
+set.undolevels = 100 -- maximum number of changes than can be undone
+set.updatetime = 250 -- if this many ms nothing is typed, write the swap file
+set.visualbell = true
+set.wildignore = {
   "*.o",
   "*.obj",
   "*.a",
@@ -90,7 +90,7 @@ opt.wildignore = {
   "**/node_modules/**",
   ".git",
 }
-opt.wildmode = { "longest", "full" }
-opt.wildoptions:remove({ "tagfile" })
-opt.wrap = false
-opt.writebackup = true -- protect against crash during write
+set.wildmode = { "longest", "full" }
+set.wildoptions:remove({ "tagfile" })
+set.wrap = false
+set.writebackup = true -- protect against crash during write
