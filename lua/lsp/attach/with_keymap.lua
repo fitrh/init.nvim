b0caf4631110368044b61e9dbff1bc90b7f75ed2
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-29 15:24:09 +0800

    [lsp.attach.with_keymap] Use loop to avoid redundant if-statement
    Provides a table that maps each capability with corresponding keymap
    definition and in the loop, checks if the capability is available.
    
    Capabilities with more than one possible function (e.g. `call_hierarchy`),
    defined with different letter case, this is why we convert each key to
    lowercase in the loop.

--- lua/lsp/attach/with_keymap.lua
+++ lua/lsp/attach/with_keymap.lua
@@ -5,68 +5,38 @@ function LspKeymap.attach(client, bufnr)
   local lsp = vim.lsp.buf
 
   local keymap = require("sugar.keymap")
-  local bind, map = keymap.bind, keymap.map
+  local map = keymap.map
   local leader, n = keymap.modifier.leader, keymap.mode.normal
 
-  if capable.code_lens then
-    bind(n(map("gc", vim.lsp.codelens.run, { buffer = bufnr })))
-  end
-
-  if capable.document_formatting then
-    bind(n(map(leader("<CR>"), function()
+  local available_keymaps = {
+    code_lens = n(map("gc", vim.lsp.codelens.run)),
+    rename = n(map("[rn", lsp.rename)),
+    signature_help = n(map("[s", lsp.signature_help)),
+    hover = n(map("K", lsp.hover)),
+    declaration = n(map("]D", lsp.declaration)),
+    goto_definition = n(map("[D", lsp.definition)),
+    type_definition = n(map("[T", lsp.type_definition)),
+    implementation = n(map("]ll", lsp.implementation)),
+    find_references = n(map("[ll", lsp.references)),
+    document_symbol = n(map("]ls", lsp.document_symbol)),
+    call_hierarchy = n(map("]li", lsp.incoming_calls)),
+    call_Hierarchy = n(map("]lo", lsp.outgoing_calls)),
+    document_formatting = n(map(leader("<CR>"), function()
       lsp.formatting_seq_sync({}, 5000)
       vim.schedule(function()
         vim.cmd("update")
       end)
-    end, { buffer = bufnr })))
+    end)),
+  }
+
+  local keymaps = {}
+  for capability, keydef in pairs(available_keymaps) do
+    if capable[(capability):lower()] then
+      table.insert(keymaps, keydef)
+    end
   end
 
-  if capable.rename then
-    bind(n(map("[rn", lsp.rename, { buffer = bufnr })))
-  end
-
-  if capable.signature_help then
-    bind(n(map("[s", lsp.signature_help, { buffer = bufnr })))
-  end
-
-  if capable.hover then
-    bind(n(map("K", lsp.hover, { buffer = bufnr })))
-  end
-
-  if capable.declaration then
-    bind(n(map("]D", lsp.declaration, { buffer = bufnr })))
-  end
-
-  if capable.goto_definition then
-    bind(n(map("[D", lsp.definition, { buffer = bufnr })))
-  end
-
-  if capable.type_definition then
-    bind(n(map("[T", lsp.type_definition, { buffer = bufnr })))
-  end
-
-  if capable.implementation then
-    bind(n(map("]ll", lsp.implementation, { buffer = bufnr })))
-  end
-
-  if capable.find_references then
-    bind(n(map("[ll", lsp.references, { buffer = bufnr })))
-  end
-
-  if capable.document_symbol then
-    bind(n(map("]ls", lsp.document_symbol, { buffer = bufnr })))
-  end
-
-  if capable.call_hierarchy then
-    bind({
-      n(map("]li", function()
-        lsp.incoming_calls()
-      end)),
-      n(map("]lo", function()
-        lsp.outgoing_calls()
-      end)),
-    }, { buffer = bufnr })
-  end
+  keymap.bind(keymaps, { buffer = bufnr })
 end
 
 return LspKeymap
