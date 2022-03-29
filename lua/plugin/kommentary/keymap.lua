2fb15d7891eef857fb09d4abd9a10cbace5dee34
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-29 13:06:12 +0800

    [plugin.kommentary.keymap] Fix wrong import and plug scriptname

--- lua/plugin/kommentary/keymap.lua
+++ lua/plugin/kommentary/keymap.lua
@@ -1,11 +1,11 @@
 local keymap = require("sugar.keymap")
 local map, modifier, mode = keymap.map, keymap.modifier, keymap.mode
-local leader, plug = modifier.leader, modifier.ex
+local leader, plug = modifier.leader, modifier.plug
 local n, v = mode.normal, mode.vselect
 
 keymap.bind({
   n(map(leader("/"), plug("kommentary_line_default"))),
-  v(map(leader("/"), plug("kommentary_line_default"))),
+  v(map(leader("/"), plug("kommentary_visual_default"))),
   n(map(leader("c"), plug("kommentary_motion_default"))),
   n(map(leader("cic"), plug("kommentary_line_increase"))),
   n(map(leader("ci"), plug("kommentary_motion_increase"))),
