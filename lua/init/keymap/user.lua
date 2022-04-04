9bc98ceb3a19c84dbacf5fa5f23d3c1ac91cf4c4
fitrh <fitrahm17h@student.unhas.ac.id>
2022-04-04 13:59:55 +0800

    [init.keymap.user] Replace user command with lua function

--- lua/init/keymap/user.lua
+++ lua/init/keymap/user.lua
@@ -1,16 +1,28 @@
 local keymap = require("sugar.keymap")
 local map, mode, modifier = keymap.map, keymap.mode, keymap.modifier
 local n = mode.normal
-local leader, ex = modifier.leader, modifier.ex
+local leader = modifier.leader
 
 keymap.bind({
   -- Minimal tmux Integration behavior
-  n(map("<M-h>", ex("TmuxMoveH"))),
-  n(map("<M-j>", ex("TmuxMoveJ"))),
-  n(map("<M-k>", ex("TmuxMoveK"))),
-  n(map("<M-l>", ex("TmuxMoveL"))),
+  n(map("<M-h>", function()
+    require("helper.tmux").move("h")
+  end)),
+  n(map("<M-j>", function()
+    require("helper.tmux").move("j")
+  end)),
+  n(map("<M-k>", function()
+    require("helper.tmux").move("k")
+  end)),
+  n(map("<M-l>", function()
+    require("helper.tmux").move("l")
+  end)),
 
   -- Minimal zen mode behavior
-  n(map(leader("zz"), ex("ZenToggleFull"))),
-  n(map(leader("nr"), ex("ZenToggle"))),
+  n(map(leader("zz"), function()
+    require("helper.zen").toggle()
+  end)),
+  n(map(leader("nr"), function()
+    require("helper.zen").toggle({ laststatus = true })
+  end)),
 })
