04eae4240a3aa15e95da760a9cccc0566b975592
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-05 23:40:07 +0800

    [plugin.trouble.keymap] Use plugin lua API

--- lua/plugin/trouble/keymap.lua
+++ lua/plugin/trouble/keymap.lua
@@ -1,14 +1,30 @@
 local key = require("sugar.keymap")
-local map, n, ex = key.map, key.mode.normal, key.modifier.ex
+local map, n = key.map, key.mode.normal
+
+local trouble = require("trouble")
 
 key.bind({
-  n(map("[xx", ex("TroubleToggle"))),
-  n(map("[xd", ex("TroubleToggle document_diagnostics"))),
-  n(map("[xw", ex("TroubleToggle workspace_diagnostics"))),
-  n(map("[xl", ex("TroubleToggle loclist"))),
-  n(map("[xq", ex("TroubleToggle quickfix"))),
-  n(map("[xr", ex("TroubleToggle lsp_references"))),
-  n(map("[xD", ex("TroubleToggle lsp_definitions"))),
-  n(map("[xt", ex("TroubleToggle lsp_type_definitions"))),
-  n(map("[xR", ex("TroubleRefresh"))),
+  n(map("[xx", trouble.toggle)),
+  n(map("[xd", function()
+    trouble.toggle("document_diagnostics")
+  end)),
+  n(map("[xw", function()
+    trouble.toggle("workspace_diagnostics")
+  end)),
+  n(map("[xl", function()
+    trouble.toggle("loclist")
+  end)),
+  n(map("[xq", function()
+    trouble.toggle("quickfix")
+  end)),
+  n(map("[xr", function()
+    trouble.toggle("lsp_references")
+  end)),
+  n(map("[xD", function()
+    trouble.toggle("lsp_definitions")
+  end)),
+  n(map("[xt", function()
+    trouble.toggle("lsp_type_definitions")
+  end)),
+  n(map("[xR", trouble.refresh)),
 })
