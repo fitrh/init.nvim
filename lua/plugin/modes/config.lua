4d9abc718446772d599ff15c74ddc9012c80d4d8
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-06 20:24:55 +0800

    [plugin.modes] Remove `colors` configuration
    4dbd584 follow up.

--- lua/plugin/modes/config.lua
+++ lua/plugin/modes/config.lua
@@ -1,12 +1,5 @@
-local palette = require("plugin.themes.palette")
 local config = {}
 
-config.colors = {
-  copy = palette.yellow.primary,
-  delete = palette.red.primary,
-  insert = palette.fg.secondary,
-  visual = palette.magenta.secondary,
-}
 config.line_opacity = { insert = 0, visual = 0.2 }
 
 require("modes").setup(config)
