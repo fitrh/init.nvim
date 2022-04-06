71955a49169dde55295a004cf4d2cab6eaae64cf
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-07 00:05:48 +0800

    [plugin.colorscheme.kanagawa] Initial configuration

---
+++ lua/plugin/colorscheme/kanagawa/config.lua
@@ -0,0 +1,55 @@
+local config = {}
+
+config.globalStatus = true
+
+require("kanagawa").setup(config)
+
+vim.cmd("colorscheme kanagawa")
+
+local c = require("kanagawa.colors").setup()
+local hi = require("sugar.highlight")
+
+---@type table<string, HighlightDef>
+local highlights = {
+  -- Builtin
+  FloatBorder = { link = "Normal" },
+  NormalFloat = { link = "Normal" },
+  TabLine = { inherit = "StatusLine", fg = c.oldWhite },
+  TabLineFill = { link = "TabLine" },
+  TabLineSel = { fg = c.fujiWhite, bg = c.sumInk1 },
+  VertSplit = { fg = hi.bg("StatusLine") },
+  WinSeparator = { link = "VertSplit" },
+
+  -- Plugin
+  ModesCopy = { bg = hi.bg("IncSearch") },
+  ModesDelete = { bg = c.autumnRed },
+  ModesInsert = { bg = c.fujiWhite },
+  ModesVisual = { bg = c.oniViolet },
+  NotifyTRACEBorder = { link = "DiagnosticHint" },
+  NotifyTRACETitle = { link = "NotifyTRACEBorder" },
+  NotifyTRACEIcon = { link = "NotifyTRACEBorder" },
+  NotifyDEBUGBorder = { fg = c.fujiGray },
+  NotifyDEBUGTitle = { link = "NotifyDEBUGBorder" },
+  NotifyDEBUGIcon = { link = "NotifyDEBUGBorder" },
+  NotifyINFOBorder = { link = "DiagnosticInfo" },
+  NotifyINFOTitle = { link = "NotifyINFOBorder" },
+  NotifyINFOIcon = { link = "NotifyINFOBorder" },
+  NotifyWARNBorder = { link = "DiagnosticWarn" },
+  NotifyWARNTitle = { link = "NotifyWARNBorder" },
+  NotifyWARNIcon = { link = "NotifyWARNBorder" },
+  NotifyERRORBorder = { link = "DiagnosticError" },
+  NotifyERRORTitle = { link = "NotifyERRORBorder" },
+  NotifyERRORIcon = { link = "NotifyERRORBorder" },
+  StatusLineGitBranch = { inherit = "StatusLine", fg = c.springViolet1 },
+  StatusLineModified = { inherit = "StatusLine", fg = c.autumnRed },
+  StatusLineMNormal = { fg = c.crystalBlue },
+  StatusLineMInsert = { fg = c.autumnGreen },
+  StatusLineMVisual = { fg = c.oniViolet },
+  StatusLineMReplace = { fg = c.autumnRed },
+  StatusLineMCommand = { fg = c.boatYellow2 },
+  StatusLineRO = { inherit = "StatusLine", fg = c.samuraiRed },
+}
+
+for group, def in pairs(highlights) do
+  hi.set(group, def)
+end
--- lua/plugin/colorscheme/packerspec.lua
+++ lua/plugin/colorscheme/packerspec.lua
@@ -1,5 +1,6 @@
 return {
   { "folke/tokyonight.nvim", as = "colorscheme/tokyonight", opt = true },
+  { "rebelot/kanagawa.nvim", as = "colorscheme/kanagawa", opt = true },
   { "marko-cerovac/material.nvim", as = "colorscheme/material", opt = true },
   { "projekt0n/github-nvim-theme", as = "colorscheme/github", opt = true },
 }
