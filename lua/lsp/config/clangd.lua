06cb0635b1cd43703e16a2f28d6e01687c0c8f02
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-08 20:06:02 +0800

    [lsp.config.clangd] Wrap with `lsp.config`

--- lua/lsp/config/clangd.lua
+++ lua/lsp/config/clangd.lua
@@ -1,27 +1,24 @@
-local handler = require("lsp.handler")
-local attach = require("lsp.attach")
-local capabilities = require("lsp.capability")
+local setup = require("lsp.config")
+local bin = "clangd"
+local config = {}
 
-return {
-  cmd = {
-    -- see clangd --help-hidden
-    "clangd",
-    -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-    -- to add more checks, create .clang-tidy file in the root directory
-    -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
-    "--clang-tidy",
-    "--completion-style=bundled",
-    "--cross-file-rename",
-    "--header-insertion=iwyu",
-  },
-  capabilities = capabilities,
-  handlers = handler.default(),
-  -- TODO: figure out what is this
-  init_options = {
-    clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
-    usePlaceholders = true,
-    completeUnimported = true,
-    semanticHighlighting = true,
-  },
-  on_attach = attach.with.all,
+config.cmd = {
+  bin, -- SEE: clangd --help-hidden for possible options
+  -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+  -- to add more `checks`, create  a `.clang-tidy` file in the root directory
+  -- SEE: https://clang.llvm.org/extra/clang-tidy
+  "--clang-tidy",
+  "--completion-style=bundled",
+  "--cross-file-rename",
+  "--header-insertion=iwyu",
 }
+
+config.init_options = {
+  clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
+  -- TODO: figure out what is this
+  usePlaceholders = true,
+  completeUnimported = true,
+  semanticHighlighting = true,
+}
+
+return setup.with(bin, config)
