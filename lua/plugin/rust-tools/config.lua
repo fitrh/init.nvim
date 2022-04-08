1ecc9221e271bfafc1f85f2a74470562010388a4
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-08 20:19:04 +0800

    [plugin.rust-tools] Wrap with `lsp.config`

--- lua/plugin/rust-tools/config.lua
+++ lua/plugin/rust-tools/config.lua
@@ -1,19 +1,25 @@
+local setup = require("lsp.config")
+local bin = "rust-analyzer"
+local config = {}
+
+config.settings = {
+  ["rust-analyzer"] = {
+    hoverActions = { references = true },
+    rustfmt = { enableRangeFormatting = true },
+  },
+}
+
+config.on_attach = function(client, bufnr)
+  require("lsp.attach").with.all(client, bufnr)
+  require("plugin.rust-tools.keymap").attach(bufnr)
+end
+
+local server = setup.with(bin, config)
+if not server then
+  return false
+end
+
 require("rust-tools").setup({
-  tools = {
-    hover_actions = { auto_focus = true },
-  },
-  server = {
-    capability = require("lsp.capability"),
-    handlers = require("lsp.handler").default(),
-    settings = {
-      ["rust-analyzer"] = {
-        hoverActions = { references = true },
-        rustfmt = { enableRangeFormatting = true },
-      },
-    },
-    on_attach = function(client, bufnr)
-      require("lsp.attach").with.all(client, bufnr)
-      require("plugin.rust-tools.keymap").attach()
-    end,
-  },
+  tools = { hover_actions = { auto_focus = true } },
+  server = server,
 })
