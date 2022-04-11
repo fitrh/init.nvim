31d1832d3ca0b6e5f4ea33e6f83c51717a3e8e11
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-12 00:46:38 +0800

    [after/ftplugin/help] Use command instead of key notation
    This is to avoid the possibility if the key notation is overridden.

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -3,4 +3,4 @@ local set = vim.opt_local
 set.colorcolumn = ""
 set.signcolumn = "yes"
 
-vim.keymap.set("n", "q", "<C-w>q", { buffer = true })
+vim.keymap.set("n", "q", ":close<CR>", { buffer = true, silent = true })
