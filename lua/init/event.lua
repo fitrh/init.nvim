c4202010605c47564d100c92dbe807e802d3680c
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-18 17:24:50 +0800

    [init.event] Save cursor position after trimming trailing whitespace

--- lua/init/event.lua
+++ lua/init/event.lua
@@ -2,7 +2,13 @@ local augroup = require("sugar.augroup")
 
 augroup("TrimAndMakeDir", {
   "BufWritePre",
-  { command = [[:%s/\s\+$//e]] },
+  {
+    callback = function()
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      vim.cmd([[:%s/\s\+$//e]])
+      vim.api.nvim_win_set_cursor(0, cursor)
+    end,
+  },
   {
     callback = function()
       require("helper.dir").mk()
