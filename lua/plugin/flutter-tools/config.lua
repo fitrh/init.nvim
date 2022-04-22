b4693ae476fa7e6e3504b08ec0e7ce36f386f75d
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-22 21:41:27 +0800

    [plugin.flutter-tools] Wrap with `lsp.config`

--- lua/plugin/flutter-tools/config.lua
+++ lua/plugin/flutter-tools/config.lua
@@ -3,31 +3,34 @@ local env = {
   FLUTTER_ROOT = os.getenv("FLUTTER_ROOT") or os.getenv("FLUTTER_SDK"),
 }
 
-require("flutter-tools").setup({
-  closing_tags = { prefix = "ﰪ " },
-  lsp = {
-    capabilities = require("lsp.capability"),
-    handlers = require("lsp.handler").default(),
-    settings = {
-      dart = {
-        devToolsBrowser = "default",
-        enableServerSnippets = true,
-        flutterSdkPath = env.FLUTTER_ROOT,
-        previewLsp = true,
-        sdkPath = env.DART_SDK,
-        renameFilesWithClasses = "prompt",
-        triggerSignatureHelpAutomatically = true,
-      },
-    },
-    on_attach = require("lsp.attach").with.all,
-    color = {
-      enabled = true,
-      foreground = false,
-      background = true,
-      virtual_text = false,
-    },
+local setup = require("lsp.config")
+local config = {}
+config.closing_tags = { prefix = "ﰪ " }
+
+config.lsp = setup.with("flutter")
+if not config.lsp then
+  return false
+end
+
+config.lsp.settings = {
+  dart = {
+    devToolsBrowser = "default",
+    enableServerSnippets = true,
+    flutterSdkPath = env.FLUTTER_ROOT,
+    previewLsp = true,
+    sdkPath = env.DART_SDK,
+    renameFilesWithClasses = "prompt",
+    triggerSignatureHelpAutomatically = true,
   },
-})
+}
+config.lsp.color = {
+  enabled = true,
+  foreground = false,
+  background = true,
+  virtual_text = false,
+}
+
+require("flutter-tools").setup(config)
 
 local ok, telescope = pcall(require, "telescope")
 if ok and telescope.load_extension then
