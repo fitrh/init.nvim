c9b4a585719321bd57660c8d70772ca12c336fc7
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-04-28 20:51:05 +0800

    [plugin.lualine.component.mode] Handle more modes

--- lua/plugin/lualine/component/mode.lua
+++ lua/plugin/lualine/component/mode.lua
@@ -1,18 +1,27 @@
 local modes = {
   n = "NRM",
+  niI = "N·I", -- i_CTRL_O
+  niR = "N·R", -- R_CTRL_O
+  niV = "NRV", -- gR_CTRL_O
   nt = "N·T",
   i = "INS",
-  ix = "I·X", -- i_CTRL-N
-  ic = "I·C", -- i_CTRL-X
+  ix = "I·X", -- i_CTRL_X
+  ic = "I·C", -- i_CTRL_N | i_CTRL_P
   v = "VIS",
   [""] = "V·B",
   V = "V·L",
+  vs = "S·V", -- gh_CTRL_O
+  Vs = "SVL", -- gH_CTRL_O
   c = "CMD",
-  s = "SEL",
-  S = "S·L",
-  [""] = "S·B",
-  R = "REP",
+  s = "SEL", -- gh
+  S = "S·L", -- gH
+  [""] = "S·B", -- g_CTRL_H
+  R = "REP", -- R
+  Rx = "R·X", -- R_CTRL_X
+  Rc = "R·C", -- R_CTRL_P
   Rv = "R·V", -- gR
+  Rvc = "RVC", -- gR_CTRL_P
+  Rvx = "RVX", -- gR_CTRL_X
   t = "TRM",
 }
 
