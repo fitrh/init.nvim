7578085dc8b72c19fa6261ef9e88153ea43b98b5
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-05-16 12:24:11 +0800

    [init.option] Add explanation comments

--- lua/init/option.lua
+++ lua/init/option.lua
@@ -4,8 +4,15 @@ set.backup = false -- but do not persist backup after successful write
 set.backupcopy = "auto" -- best option for backup strategy
 set.breakindent = true
 set.colorcolumn = { "+1" } -- draw a vertical ruler at (textwidth + 1)th column
-set.complete:remove({ "u", "t" })
-set.completeopt:append({ "menuone", "noinsert" })
+set.complete:remove({
+  -- disable scan for
+  "u", -- unload buffers
+  "t", -- tag completion
+})
+set.completeopt:append({
+  "menuone", -- use the popup menu also when there is only one match
+  "noinsert", -- do not insert any text for a match until the user selects a match from the menu
+})
 set.conceallevel = 2 -- hide concealed text unless it has a custom replacement
 set.confirm = true -- raise a dialogue asking if you wish to save changed files
 set.cursorline = true -- highlight current line
@@ -20,39 +27,39 @@ set.fillchars = {
   verthoriz = "╋",
   eob = " ", -- use 'space' for lines after the last buffer line in a window
 }
-set.guicursor = "n-c:hor50,i-ci-ve:ver25"
+set.guicursor = "n-c:hor50,i-ci-ve:ver25" -- horizontal in normal, vertical in insert
 set.hidden = true -- enable switching unsaved buffer
-set.ignorecase = true
+set.ignorecase = true -- use case-insensitive in search
 set.laststatus = 0 -- disable statusline
 set.nrformats:append({ "alpha" }) -- increment/decrement for alphabet
-set.number = true
-set.numberwidth = 1
+set.number = true -- enable line number
+set.numberwidth = 1 -- default number of columns to use for line number
 set.pastetoggle = "<F2>" -- toggling paste mode when press this key
-set.path:append({ "**" })
+set.path:append({ "**" }) -- search in current directory recursively
 set.pumblend = 10 -- pseudo-transparency for the popup-menu, value : 0 - 100
 set.pumheight = 10 -- maximum number of items to show in the pop-up menu
-set.relativenumber = true
+set.relativenumber = true -- show line number relative to the line with the cursor
 set.ruler = false -- hide line & column information, use g Ctrl + g instead
 set.scrolloff = 10 -- minimal number of line to keep around the cursor
-set.shell = "/usr/bin/bash"
-set.shiftwidth = 4
-set.shortmess = {
-  a = true,
-  t = true,
-  T = true,
-  c = true,
-  F = true,
+set.shell = "/usr/bin/bash" -- use absolute path to avoid using $SHELL
+set.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent
+set.shortmess = { -- shorten message in prompt window
+  a = true, -- enable 'filmnrwx' flag
+  t = true, -- truncate file message at the start
+  T = true, -- truncate other messages in the middle
+  c = true, -- don't give ins-completion-menu messages
+  F = true, -- don't give the file info when editing a file
 }
-set.showcmd = false
+set.showcmd = false -- hide (partial) command in the last line of the screen
 set.showmatch = true -- highlight matching <[{()}]>
-set.showmode = false
+set.showmode = false -- hide current mode in command-line window
 set.showtabline = 1 -- show tab-page only if there are at least 2 tab pages
 set.sidescrolloff = 10 -- minimal number of column to keep around the cursor
-set.signcolumn = "yes"
-set.smartcase = true
-set.softtabstop = 4
-set.splitbelow = true
-set.splitright = true
+set.signcolumn = "yes" -- always show signcolumn to avoid shifting line number
+set.smartcase = true -- use case-sensitive when the search term contains an uppercase letter
+set.softtabstop = 4 -- number of spaces that a <Tab> counts for
+set.splitbelow = true -- when :split, put the new window below the current one
+set.splitright = true -- when :vsplit, put the new window right of the current one
 set.synmaxcol = 120 -- do syntax highlighting until synmaxcol character
 set.termguicolors = true -- enable 24-bit RGB color in the TUI
 set.textwidth = 80 -- set 80 character as default line length
@@ -60,7 +67,7 @@ set.undofile = true -- save undo history to an undo file on BufWrite
 set.undolevels = 100 -- maximum number of changes than can be undone
 set.updatetime = 250 -- if this many ms nothing is typed, write the swap file
 set.visualbell = true
-set.wildignore = {
+set.wildignore = { -- list of file patterns to ignore when expanding wildcards
   "*.o",
   "*.obj",
   "*.a",
@@ -89,5 +96,5 @@ set.wildignore = {
 }
 set.wildmode = { "longest", "full" }
 set.wildoptions:remove({ "tagfile" })
-set.wrap = false
+set.wrap = false -- disable line wrap
 set.writebackup = true -- protect against crash during write
