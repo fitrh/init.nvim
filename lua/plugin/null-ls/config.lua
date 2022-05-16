c1cbad336f4bd8c58a7e5389655d8e48a0646797
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-05-16 17:26:06 +0800

    [plugin.null-ls] Restructure configuration

--- lua/plugin/null-ls/config.lua
+++ lua/plugin/null-ls/config.lua
@@ -1,63 +1,74 @@
 local null_ls = require("null-ls")
-local f = null_ls.builtins.formatting
-local d = null_ls.builtins.diagnostics
-local a = null_ls.builtins.code_actions
 local c = require("plugin.null-ls.condition")
 
 local js = { "svelte" }
 local plaintext = { "gitcommit", "markdown", "txt" }
 
-null_ls.setup({
-  on_attach = require("lsp.attach").with.all,
-  sources = {
-    -- formatter
-    f.black.with({
-      extra_args = {
-        "--line-length",
-        vim.api.nvim_buf_get_option(0, "textwidth"),
-      },
-    }),
-    f.brittany,
-    f.djlint.with({ condition = c.djlint }),
-    f.fish_indent,
-    f.isort.with({ extra_args = { "--profile", "black" } }),
-    f.markdownlint,
-    f.phpcbf,
-    f.prettierd.with({ condition = c.prettier, extra_filetypes = js }),
-    f.shellharden,
-    f.shfmt.with({ extra_args = { "-i", "4", "-ci" } }),
-    f.stylelint.with({ condition = c.stylelint }),
-    f.stylua.with({ condition = c.stylua }),
+local formatter = {
+  black = { extra_args = { "--line-length", vim.bo.textwidth } },
+  brittany = {},
+  djlint = { condition = c.djlint },
+  fish_indent = {},
+  isort = { extra_args = { "--profile", "black" } },
+  markdownlint = {},
+  phpcbf = {},
+  prettierd = { condition = c.prettier, extra_filetypes = js },
+  shellharden = {},
+  shfmt = { extra_args = { "-i", "4", "-ci" } },
+  stylelint = { condition = c.stylelint },
+  stylua = { condition = c.stylua },
+}
 
-    -- linter
-    d.cppcheck.with({
-      condition = c.cppcheck,
-      extra_args = { "--cppcheck-build-dir=.cppcheck" },
-    }),
-    d.djlint.with({ condition = c.djlint }),
-    d.eslint_d.with({ condition = c.eslint, extra_filetypes = js }),
-    d.fish,
-    d.flake8,
-    d.golangci_lint.with({ condition = c.golangci }),
-    d.markdownlint,
-    d.misspell.with({ filetypes = plaintext }),
-    d.mypy.with({ condition = c.mypy }),
-    d.phpcs,
-    d.phpstan.with({ condition = c.phpstan }),
-    d.pylint.with({ condition = c.pylint }),
-    d.revive.with({
-      args = { "-config", "revive.toml", "-formatter", "json", "./..." },
-      condition = c.revive,
-    }),
-    d.selene.with({ condition = c.selene }),
-    d.shellcheck,
-    d.staticcheck.with({ condition = c.staticcheck }),
-    d.stylelint.with({ condition = c.stylelint }),
-    d.write_good.with({ filetypes = plaintext }),
-
-    -- code action
-    a.eslint_d.with({ condition = c.eslint, extra_filetypes = js }),
-    a.gitrebase,
-    a.shellcheck,
+local diagnostic = {
+  cppcheck = {
+    condition = c.cppcheck,
+    extra_args = { "--cppcheck-build-dir=.cppcheck" },
   },
+  djlint = { condition = c.djlint },
+  eslint_d = { condition = c.eslint, extra_filetypes = js },
+  fish = {},
+  flake8 = {},
+  golangci_lint = { condition = c.golangci },
+  markdownlint = {},
+  misspell = { filetypes = plaintext },
+  mypy = { condition = c.mypy },
+  phpcs = {},
+  phpstan = { condition = c.phpstan },
+  pylint = { condition = c.pylint },
+  revive = {
+    args = { "-config", "revive.toml", "-formatter", "json", "./..." },
+    condition = c.revive,
+  },
+  selene = { condition = c.selene },
+  shellcheck = {},
+  staticcheck = { condition = c.staticcheck },
+  stylelint = { condition = c.stylelint },
+  write_good = { filetypes = plaintext },
+}
+
+local code_action = {
+  eslint_d = { condition = c.eslint, extra_filetypes = js },
+  gitrebase = {},
+  shellcheck = {},
+}
+
+local registered_sources = {}
+for builtin, sources in pairs({
+  formatting = formatter,
+  diagnostics = diagnostic,
+  code_actions = code_action,
+}) do
+  for source, config in pairs(sources) do
+    table.insert(
+      registered_sources,
+      null_ls.builtins[builtin][source].with(config)
+    )
+  end
+end
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    require("lsp.attach").with.all(client, bufnr)
+  end,
+  sources = registered_sources,
 })
