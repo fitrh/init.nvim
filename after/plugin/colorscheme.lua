0a7e93caf93874004d8803e79a763ab320a9d970
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-05-27 23:28:02 +0800

    [after/plugin/colorscheme] Restructure and add guards for empty values

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,8 +1,14 @@
-local colorscheme = os.getenv("NVIM_COLORSCHEME") or "tokyonight"
 local group = vim.api.nvim_create_augroup("UseColorScheme", {})
 vim.api.nvim_create_autocmd("VimEnter", {
   group = group,
   callback = function()
+    local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME")
+    local colorscheme = "tokyonight"
+
+    if NVIM_COLORSCHEME and NVIM_COLORSCHEME ~= "" then
+      colorscheme = NVIM_COLORSCHEME
+    end
+
     vim.cmd(("packadd colorscheme/%s"):format(colorscheme))
     require(("plugin.colorscheme.%s.config"):format(colorscheme))
     vim.api.nvim_exec_autocmds(("ColorScheme %s"):format(colorscheme), {})
