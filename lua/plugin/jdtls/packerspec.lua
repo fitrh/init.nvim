6018b544763d6f3f0dbe40ab3a370b328f75c24d
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-05-27 22:22:21 +0800

    [plugin.jdtls.packerspec] Add callback function for `config`

--- lua/plugin/jdtls/packerspec.lua
+++ lua/plugin/jdtls/packerspec.lua
@@ -4,6 +4,11 @@ return {
     local command = require("sugar.command")
     local event = require("sugar.event")
     local config = require("plugin.jdtls.config")
+      or function()
+        vim.notify("JDT.LS Configuration Failed", vim.log.levels.WARN, {
+          title = "nvim-jdtls",
+        })
+      end
 
     command.add("JdtStart", config)
     event.augroup("AttachJDTLS", { event.autocmd("FileType", "java", config) })
