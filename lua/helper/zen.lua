90db822b6627a1836822e7f147c6ef3d998061ec
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-01 17:22:25 +0800

    [help.zen] Use `state.zen`

--- lua/helper/zen.lua
+++ lua/helper/zen.lua
@@ -1,13 +1,15 @@
 local M = {}
 
 local state = {
+  zen = false,
   laststatus = vim.api.nvim_get_option("laststatus"),
+  signcolumn = vim.api.nvim_win_get_option(0, "signcolumn"),
 }
 
 function M.toggle(args)
   local opts = args or {}
 
-  if vim.api.nvim_win_get_option(0, "signcolumn") == "yes" then
+  if not state.zen then
     vim.api.nvim_win_set_option(0, "signcolumn", "no")
     vim.api.nvim_win_set_option(0, "number", false)
     vim.api.nvim_win_set_option(0, "relativenumber", false)
@@ -15,13 +17,15 @@ function M.toggle(args)
       vim.api.nvim_set_option("laststatus", 0)
     end
   else
-    vim.api.nvim_win_set_option(0, "signcolumn", "yes")
+    vim.api.nvim_win_set_option(0, "signcolumn", state.signcolumn)
     vim.api.nvim_win_set_option(0, "number", true)
     vim.api.nvim_win_set_option(0, "relativenumber", true)
     if opts.laststatus then
       vim.api.nvim_set_option("laststatus", state.laststatus)
     end
   end
+
+  state.zen = not state.zen
 end
 
 return M
