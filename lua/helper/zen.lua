b0e4621c2fc4df640c473a076eab3e9cdd255bef
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-01 22:53:38 +0800

    [helper.zen] Initial implementation of left-right padding

--- lua/helper/zen.lua
+++ lua/helper/zen.lua
@@ -1,37 +1,130 @@
 local M = {}
 
+local api = vim.api
+
 local state = {
   zen = false,
-  laststatus = vim.api.nvim_get_option("laststatus"),
-  colorcolumn = vim.api.nvim_win_get_option(0, "colorcolumn"),
-  showtabline = vim.api.nvim_get_option("showtabline"),
-  signcolumn = vim.api.nvim_win_get_option(0, "signcolumn"),
+  colorcolumn = api.nvim_win_get_option(0, "colorcolumn"),
+  laststatus = api.nvim_get_option("laststatus"),
+  showtabline = api.nvim_get_option("showtabline"),
+  signcolumn = api.nvim_win_get_option(0, "signcolumn"),
 }
 
-function M.toggle(args)
-  local opts = args or {}
+local option = {}
 
-  if not state.zen then
-    vim.api.nvim_set_option("showtabline", 0)
-    vim.api.nvim_win_set_option(0, "colorcolumn", "")
-    vim.api.nvim_win_set_option(0, "signcolumn", "no")
-    vim.api.nvim_win_set_option(0, "number", false)
-    vim.api.nvim_win_set_option(0, "relativenumber", false)
-    if opts.laststatus then
-      vim.api.nvim_set_option("laststatus", 0)
-    end
-  else
-    vim.api.nvim_set_option("showtabline", state.showtabline)
-    vim.api.nvim_win_set_option(0, "colorcolumn", state.colorcolumn)
-    vim.api.nvim_win_set_option(0, "signcolumn", state.signcolumn)
-    vim.api.nvim_win_set_option(0, "number", true)
-    vim.api.nvim_win_set_option(0, "relativenumber", true)
-    if opts.laststatus then
-      vim.api.nvim_set_option("laststatus", state.laststatus)
+option.global = {
+  { name = "showtabline", disable_value = 0 },
+}
+
+option.window = {
+  { name = "colorcolumn", disable_value = "" },
+  { name = "number", disable_value = false },
+  { name = "relativenumber", disable_value = false },
+  { name = "signcolumn", disable_value = "no" },
+}
+
+---Manage option for zen window
+---@param zen boolean @zen mode active state
+---@param scope boolean|table @manage global and window option
+local function opt(zen, scope)
+  scope = scope or { global = true, win = true }
+  if type(scope) == "boolean" then
+    scope = { global = scope, win = scope }
+  end
+
+  if scope.global then
+    for _, global in ipairs(option.global) do
+      local value = zen and global.disable_value
+        or state[global.name]
+        or not zen
+      api.nvim_set_option(global.name, value)
     end
   end
 
-  state.zen = not state.zen
+  if scope.win then
+    for _, win in ipairs(option.window) do
+      local value = zen and win.disable_value or state[win.name] or not zen
+      api.nvim_win_set_option(0, win.name, value)
+    end
+  end
+end
+
+-- window padding state
+local pad = {
+  buf = { left = nil, right = nil },
+  win = { left = nil, right = nil },
+  cmd = { left = "leftabove", right = "rightbelow" },
+  width = 0,
+  winhl = "VertSplit:WinSeparatorZen,WinSeparator:WinSeparatorZen",
+}
+
+local function zen(enter)
+  local columns = api.nvim_get_option("columns")
+  local textwidth = api.nvim_buf_get_option(0, "textwidth")
+
+  if enter then
+    local cursorpos = api.nvim_win_get_cursor(0)
+    -- open current window as new tabpage, before current tabpage
+    -- when we exit zen mode, we return to the previous tabpage
+    vim.cmd("-tabe %")
+    local main = {
+      win = api.nvim_get_current_win(),
+      buf = api.nvim_buf_get_number(0),
+    }
+    api.nvim_win_set_option(main.win, "winhighlight", pad.winhl)
+    api.nvim_win_set_cursor(main.win, cursorpos)
+    vim.cmd("normal! zz")
+    pad.width = math.floor(columns / 5)
+    opt(enter)
+
+    if columns > textwidth then
+      -- create left-right padding window
+      for side, cmd in pairs(pad.cmd) do
+        vim.cmd(("%s %dvnew"):format(cmd, pad.width))
+        pad.buf[side] = api.nvim_buf_get_number(0)
+        pad.win[side] = api.nvim_get_current_win()
+        api.nvim_win_set_option(pad.win[side], "winhighlight", pad.winhl)
+        opt(enter, { win = true })
+        api.nvim_set_current_win(main.win)
+      end
+
+      local main_win_width = api.nvim_win_get_width(main.win)
+      local main_buf_textwidth = api.nvim_buf_get_option(main.buf, "textwidth")
+      -- main window must have a width at least equal to 'textwidth'
+      if main_win_width < main_buf_textwidth then
+        local delta = main_buf_textwidth - main_win_width
+        pad.width = math.floor((2 * pad.width - delta) / 2)
+        for _, id in pairs(pad.win) do
+          api.nvim_win_set_width(id, pad.width)
+        end
+      end
+    end
+  else
+    local cursorpos = api.nvim_win_get_cursor(0)
+    if (columns > textwidth) and pad.buf.left and pad.buf.right then
+      for _, id in pairs(pad.buf) do
+        api.nvim_buf_delete(id, { force = true })
+      end
+    end
+    vim.cmd("tabclose")
+    opt(false)
+    api.nvim_win_set_cursor(0, cursorpos)
+  end
+end
+
+function M.toggle(args)
+  local opts = args or {}
+  local active = not state.zen
+
+  zen(active)
+
+  if opts.laststatus then
+    api.nvim_set_option("laststatus", active and 0 or state.laststatus)
+  elseif state.zen then
+    api.nvim_set_option("laststatus", state.laststatus)
+  end
+
+  state.zen = active
 end
 
 return M
