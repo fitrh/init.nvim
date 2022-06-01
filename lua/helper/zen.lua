4fdbbd1c74d9f4c69f56b5b250d282f58377b062
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-01 17:26:11 +0800

    [helper.zen] Add `showtabline` and `colorcolumn` options

--- lua/helper/zen.lua
+++ lua/helper/zen.lua
@@ -3,6 +3,8 @@ local M = {}
 local state = {
   zen = false,
   laststatus = vim.api.nvim_get_option("laststatus"),
+  colorcolumn = vim.api.nvim_win_get_option(0, "colorcolumn"),
+  showtabline = vim.api.nvim_get_option("showtabline"),
   signcolumn = vim.api.nvim_win_get_option(0, "signcolumn"),
 }
 
@@ -10,6 +12,8 @@ function M.toggle(args)
   local opts = args or {}
 
   if not state.zen then
+    vim.api.nvim_set_option("showtabline", 0)
+    vim.api.nvim_win_set_option(0, "colorcolumn", "")
     vim.api.nvim_win_set_option(0, "signcolumn", "no")
     vim.api.nvim_win_set_option(0, "number", false)
     vim.api.nvim_win_set_option(0, "relativenumber", false)
@@ -17,6 +21,8 @@ function M.toggle(args)
       vim.api.nvim_set_option("laststatus", 0)
     end
   else
+    vim.api.nvim_set_option("showtabline", state.showtabline)
+    vim.api.nvim_win_set_option(0, "colorcolumn", state.colorcolumn)
     vim.api.nvim_win_set_option(0, "signcolumn", state.signcolumn)
     vim.api.nvim_win_set_option(0, "number", true)
     vim.api.nvim_win_set_option(0, "relativenumber", true)
