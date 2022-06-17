a2d3c3a0b99944d4d63a919716e5bd506a6950c8
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-17 16:38:00 +0800

    config.lsp.server.pyright: Set `stubPath` and `useLibraryCodeForTypes`
    `stubPath` default to `PYTHON_TYPE_STUBS` environment variable.

--- lua/config/lsp/server/pyright.lua
+++ lua/config/lsp/server/pyright.lua
@@ -2,7 +2,9 @@ return require("config.lsp.server").with("pyright-langserver", {
   settings = {
     python = {
       analysis = {
+        stubPath = os.getenv("PYTHON_TYPE_STUBS") or "",
         typeCheckingMode = "basic",
+        useLibraryCodeForTypes = true,
       },
       venvPath = ".",
     },
