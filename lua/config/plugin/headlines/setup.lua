11e930d273bb85bb4512674570bfe774f3194a03
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-24 16:55:01 +0800

    config.plugin.headlines: Configure highlight groups for markdown

--- lua/config/plugin/headlines/setup.lua
+++ lua/config/plugin/headlines/setup.lua
@@ -1,6 +1,14 @@
 local config = {}
 
 config.markdown = {
+  headline_highlights = {
+    "Headline",
+    "Headline2",
+    "Headline3",
+    "Headline4",
+    "Headline5",
+    "Headline6",
+  },
   dash_string = "━",
   fat_headlines = false,
 }
