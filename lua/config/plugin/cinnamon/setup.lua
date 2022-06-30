74e86a21151eebe8012d24563ecd7fd13647f5da
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-30 17:27:46 +0800

    config.plugin.cinnamon: Workaround to disable arrow and page keys
    Without `override_keymaps`, `<C-d>/<C-u>` won't work, but enabling
    `override_keymaps` means overriding `config.keymap.builtin` where
    I defined to disable arrow and page keys.
    
    The workaround is to put the disable logic after `setup()`.

--- lua/config/plugin/cinnamon/setup.lua
+++ lua/config/plugin/cinnamon/setup.lua
@@ -5,3 +5,9 @@ require("cinnamon").setup({
   centered = false,
   scroll_limit = vim.api.nvim_get_option("lines") * 2,
 })
+
+for _, k in ipairs({ "Up", "Down", "Left", "Right", "PageUp", "PageDown" }) do
+  for _, m in ipairs({ "", "i", "c", "l", "!", "t" }) do
+    vim.api.nvim_set_keymap(m, ("<%s>"):format(k), "", {})
+  end
+end
