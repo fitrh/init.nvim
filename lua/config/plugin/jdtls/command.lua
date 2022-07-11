bb321020eab54f465e854a632801181507dc988f
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-07-11 20:37:19 +0800

    config.plugin.jdtls.command: Add update and build projects
    - JdtUpdateProjectsConfig [update_projects_config][1]
    - JdtBuildProjects [build_projects][2]
    
    [1]: https://github.com/mfussenegger/nvim-jdtls/pull/282
    [2]: https://github.com/mfussenegger/nvim-jdtls/commit/703268d5b8479de4e0c27af93b203d56c1b53d2b

--- lua/config/plugin/jdtls/command.lua
+++ lua/config/plugin/jdtls/command.lua
@@ -5,6 +5,13 @@ function M.attach(jdtls, bufnr)
     prefix = "Jdt",
     buf = bufnr,
     cmds = {
+      {
+        name = "BuildProjects",
+        opts = { nargs = "?" },
+        cmd = function(opts)
+          jdtls.build_projects(opts.fargs[1])
+        end,
+      },
       { name = "OrganizeImports", cmd = jdtls.organize_imports },
       { name = "ExtractVariable", cmd = jdtls.extract_variable },
       {
@@ -36,6 +43,13 @@ function M.attach(jdtls, bufnr)
         name = "TestNearestMethod",
         cmd = function()
           jdtls.test_nearest_method(true)
+        end,
+      },
+      {
+        name = "UpdateProjectsConfig",
+        opts = { nargs = "?" },
+        cmd = function(opts)
+          jdtls.update_projects_config(opts.fargs[1])
         end,
       },
     },
