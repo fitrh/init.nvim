87492d9433fc3ddd762ee9af4dda0a439df37398
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-07-11 16:23:37 +0800

    after/ftplugin/fish: Migrate to lua

---
+++ after/ftplugin/fish.lua
@@ -0,0 +1,9 @@
+local b = vim.b
+b.format_on_save = true
+
+local set = vim.opt_local
+set.comments = ":#"
+set.commentstring = "#%s"
+set.softtabstop = 4
+set.shiftwidth = 4
+set.textwidth = 79
--- after/ftplugin/fish.vim
+++
@@ -1,5 +0,0 @@
-setlocal comments=:#
-setlocal commentstring=#%s
-setlocal softtabstop=4
-setlocal shiftwidth=4
-setlocal textwidth=79
