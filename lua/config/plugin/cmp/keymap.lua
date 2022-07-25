e6a119b5a00db8c1dea56110e0572105120f4154
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-07-25 17:40:08 +0800

    config.plugin.cmp.keymap: `c_CTRL-{p,n}` act as `<Up>|<Down>` if no item

--- lua/config/plugin/cmp/keymap.lua
+++ lua/config/plugin/cmp/keymap.lua
@@ -3,15 +3,30 @@ local M = {}
 function M.setup(cmp, snippet)
   local map = cmp.mapping
   local select_only = { behavior = cmp.SelectBehavior.Select }
+  local function termcode(key)
+    return vim.api.nvim_replace_termcodes(key, true, true, true)
+  end
 
   return {
     ["<C-p>"] = map({
       i = map.select_prev_item(select_only),
-      c = map.select_prev_item(),
+      c = function()
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          vim.api.nvim_feedkeys(termcode("<Up>"), "n", true)
+        end
+      end,
     }),
     ["<C-n>"] = map({
       i = map.select_next_item(select_only),
-      c = map.select_next_item(),
+      c = function()
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          vim.api.nvim_feedkeys(termcode("<Down>"), "n", true)
+        end
+      end,
     }),
     ["<C-b>"] = map.scroll_docs(-4),
     ["<C-f>"] = map.scroll_docs(4),
