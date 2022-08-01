a0808c195a7e3f80a2bf31ff991c707475f952b3
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-08-01 22:17:59 +0800

    config.plugin.headlines: Disable markdown headline highlights

--- lua/config/plugin/headlines/setup.lua
+++ lua/config/plugin/headlines/setup.lua
@@ -1,14 +1,7 @@
 local config = {}
 
 config.markdown = {
-  headline_highlights = {
-    "Headline",
-    "Headline2",
-    "Headline3",
-    "Headline4",
-    "Headline5",
-    "Headline6",
-  },
+  headline_highlights = {},
   dash_string = "━",
   quote_string = "▓",
   fat_headlines = false,
