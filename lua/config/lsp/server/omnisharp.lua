2a165d2e612f29b008ec8d8dee53b76f0fc58fdf
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-08-11 20:13:39 +0800

    config.lsp.server.omnisharp: Use `OmniSharp` bin file

--- lua/config/lsp/server/omnisharp.lua
+++ lua/config/lsp/server/omnisharp.lua
@@ -2,7 +2,7 @@ if not os.getenv("DOTNET_ROOT") then
   return false
 end
 
-local bin = ("%s/omnisharp/run"):format(os.getenv("DOTNET_ROOT"))
+local bin = ("%s/omnisharp/OmniSharp"):format(os.getenv("DOTNET_ROOT"))
 
 return require("config.lsp.server").with(bin, {
   cmd = {
