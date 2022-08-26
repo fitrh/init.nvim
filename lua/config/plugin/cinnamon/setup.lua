8a11aaee9d9e7ae22a1eafaf1021430ca5f57c3d
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-08-26 21:01:55 +0800

    config.plugin.cinnamon: Enable `centered`

--- lua/config/plugin/cinnamon/setup.lua
+++ lua/config/plugin/cinnamon/setup.lua
@@ -2,7 +2,6 @@ require("cinnamon").setup({
   extra_keymaps = true,
   extended_keymaps = true,
   override_keymaps = true,
-  centered = false,
   scroll_limit = vim.api.nvim_get_option("lines") * 2,
 })
 
