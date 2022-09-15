58bd273609af13dfa948e4d41f06055c8d873221
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-09-15 20:26:27 +0800

    config.plugin.trouble.keymap: Move `require` to callback function

--- lua/config/plugin/trouble/keymap.lua
+++ lua/config/plugin/trouble/keymap.lua
@@ -1,30 +1,32 @@
 local key = require("sugar.keymap")
 local map, n = key.map, key.mode.normal
 
-local trouble = require("trouble")
-
 key.bind({
-  n(map("[xx", trouble.toggle)),
+  n(map("[xx", function()
+    require("trouble").toggle()
+  end)),
   n(map("[xd", function()
-    trouble.toggle("document_diagnostics")
+    require("trouble").toggle("document_diagnostics")
   end)),
   n(map("[xw", function()
-    trouble.toggle("workspace_diagnostics")
+    require("trouble").toggle("workspace_diagnostics")
   end)),
   n(map("[xl", function()
-    trouble.toggle("loclist")
+    require("trouble").toggle("loclist")
   end)),
   n(map("[xq", function()
-    trouble.toggle("quickfix")
+    require("trouble").toggle("quickfix")
   end)),
   n(map("[xr", function()
-    trouble.toggle("lsp_references")
+    require("trouble").toggle("lsp_references")
   end)),
   n(map("[xD", function()
-    trouble.toggle("lsp_definitions")
+    require("trouble").toggle("lsp_definitions")
   end)),
   n(map("[xt", function()
-    trouble.toggle("lsp_type_definitions")
+    require("trouble").toggle("lsp_type_definitions")
   end)),
-  n(map("[xR", trouble.refresh)),
+  n(map("[xR", function()
+    require("trouble").refresh()
+  end)),
 })
