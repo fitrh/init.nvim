a1612460fae57a2605d212870699d57854fbd610
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-10-14 21:47:03 +0800

    config.plugin.devicons.packerspec: Update location string

--- lua/config/plugin/devicons/packerspec.lua
+++ lua/config/plugin/devicons/packerspec.lua
@@ -1,7 +1,7 @@
 return {
-    "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("config.plugin.devicons.setup")
-    end,
-    module = "nvim-web-devicons",
+  "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("config.plugin.devicons.setup")
+  end,
+  module = "nvim-web-devicons",
 }
