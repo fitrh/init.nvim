4c868a8dff83f4fedd70c4f794743fc22c70075f
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-10-20 23:47:23 +0800

    after/ftplugin/python: Remove telescope mapping

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -2,9 +2,3 @@ local set = vim.opt_local
 set.shiftwidth = 4
 set.tabstop = 4
 set.textwidth = 79
-
-local telescope = {}
-telescope.ok, telescope.module = pcall(require, "telescope.builtin")
-if telescope.ok then
-  vim.keymap.set("n", "gb", telescope.module.buffers, { buffer = 0 })
-end
