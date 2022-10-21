371cf2284afc4afab0b52c71d7f07ea403b109d2
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-10-21 18:06:47 +0800

    config.event: Use snake_case for augroup name and sort alphabetically

--- lua/config/event.lua
+++ lua/config/event.lua
@@ -10,7 +10,45 @@ augroup("adaptive_scrolloff", function(autocmd)
   end)
 end)
 
-augroup("TrimAndMakeDir", function(autocmd)
+augroup("always_relativenumber", function(autocmd)
+  autocmd("OptionSet", "number", function()
+    vim.api.nvim_win_set_option(0, "relativenumber", vim.v.option_new == "1")
+  end)
+end)
+
+augroup("cursorline_on_current_window", function(autocmd)
+  autocmd({ "BufWinEnter", "WinEnter" }, "*", function()
+    vim.api.nvim_win_set_option(0, "cursorline", not vim.w.nocursorline)
+  end)
+
+  autocmd("WinLeave", "*", function()
+    if not vim.b.keep_cursor_on_leave then
+      vim.api.nvim_win_set_option(0, "cursorline", false)
+    end
+  end)
+end)
+
+augroup("highlight_postyank", function(autocmd)
+  autocmd("TextYankPost", "*", function()
+    vim.highlight.on_yank()
+  end)
+end)
+
+augroup("macro_recording", function(autocmd)
+  local opts = { title = "Macro", icon = "", timeout = 250 }
+
+  autocmd("RecordingEnter", "*", function()
+    local msg = (" 壘Recording @%s"):format(vim.fn.reg_recording())
+    vim.notify(msg, vim.log.levels.INFO, opts)
+  end)
+
+  autocmd("RecordingLeave", "*", function()
+    local msg = ("  Recorded @%s"):format(vim.v.event.regname)
+    vim.notify(msg, vim.log.levels.INFO, opts)
+  end)
+end)
+
+augroup("prewrite_action", function(autocmd)
   autocmd("BufWritePre", "*", {
     function()
       local cursor = vim.api.nvim_win_get_cursor(0)
@@ -23,25 +61,7 @@ augroup("TrimAndMakeDir", function(autocmd)
   })
 end)
 
-augroup("HighlightOnYank", function(autocmd)
-  autocmd("TextYankPost", "*", function()
-    vim.highlight.on_yank()
-  end)
-end)
-
-augroup("CursorlineOnCurrentWindow", function(autocmd)
-  autocmd({ "BufWinEnter", "WinEnter" }, "*", function()
-    vim.api.nvim_win_set_option(0, "cursorline", not vim.w.nocursorline)
-  end)
-
-  autocmd("WinLeave", "*", function()
-    if not vim.b.keep_cursor_on_leave then
-      vim.api.nvim_win_set_option(0, "cursorline", false)
-    end
-  end)
-end)
-
-augroup("RelativeNumberOnCurrentWindow", function(autocmd)
+augroup("relativenumber_on_current_window", function(autocmd)
   autocmd({ "BufWinEnter", "WinEnter" }, "*", function()
     if vim.api.nvim_win_get_option(0, "number") then
       vim.api.nvim_win_set_option(0, "relativenumber", true)
@@ -53,23 +73,7 @@ augroup("RelativeNumberOnCurrentWindow", function(autocmd)
   end)
 end)
 
-augroup("AlwaysRelativeNumber", function(autocmd)
-  autocmd("OptionSet", "number", function()
-    local v = vim.api.nvim_win_get_option(0, "number")
-    vim.api.nvim_win_set_option(0, "relativenumber", v)
-  end)
-end)
-
-augroup("OnTerminalBuffer", function(autocmd)
-  autocmd("TermOpen", "*", function()
-    if vim.api.nvim_buf_get_option(0, "filetype") == "" then
-      vim.api.nvim_buf_set_option(0, "filetype", "terminal")
-    end
-    vim.cmd.startinsert()
-  end)
-end)
-
-augroup("SetTabLine", function(autocmd)
+augroup("set_tabline", function(autocmd)
   local tabline = [[%{%v:lua.require("config.option.tabline").draw()%}]]
 
   autocmd("UIEnter", "*", function()
@@ -83,7 +87,22 @@ augroup("SetTabLine", function(autocmd)
   end)
 end)
 
-augroup("SyncWithTextwidth", function(autocmd)
+augroup("shiftwidth_sync", function(autocmd)
+  autocmd("OptionSet", "shiftwidth", function()
+    vim.api.nvim_buf_set_option(0, "tabstop", tonumber(vim.v.option_new))
+  end)
+end)
+
+augroup("terminalbuffer_action", function(autocmd)
+  autocmd("TermOpen", "*", function()
+    if vim.api.nvim_buf_get_option(0, "filetype") == "" then
+      vim.api.nvim_buf_set_option(0, "filetype", "terminal")
+    end
+    vim.cmd.startinsert()
+  end)
+end)
+
+augroup("textwidth_sync", function(autocmd)
   autocmd("OptionSet", "textwidth", function()
     vim.api.nvim_buf_set_option(0, "synmaxcol", tonumber(vim.v.option_new))
   end)
@@ -91,25 +110,5 @@ augroup("SyncWithTextwidth", function(autocmd)
   autocmd({ "BufNewFile", "BufEnter" }, "*", function()
     local textwidth = vim.api.nvim_buf_get_option(0, "textwidth")
     vim.api.nvim_buf_set_option(0, "synmaxcol", tonumber(textwidth))
-  end)
-end)
-
-augroup("SyncWithShiftwidth", function(autocmd)
-  autocmd("OptionSet", "shiftwidth", function()
-    vim.api.nvim_buf_set_option(0, "tabstop", tonumber(vim.v.option_new))
-  end)
-end)
-
-augroup("MacroRecording", function(autocmd)
-  local opts = { title = "Macro", icon = "", timeout = 250 }
-
-  autocmd("RecordingEnter", "*", function()
-    local msg = (" 壘Recording @%s"):format(vim.fn.reg_recording())
-    vim.notify(msg, vim.log.levels.INFO, opts)
-  end)
-
-  autocmd("RecordingLeave", "*", function()
-    local msg = ("  Recorded @%s"):format(vim.v.event.regname)
-    vim.notify(msg, vim.log.levels.INFO, opts)
   end)
 end)
