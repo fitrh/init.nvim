1d49be45eb2616ad8b56635c97b823ae02b3068a
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-11-04 23:04:04 +0800

    config.plugin.notify: Format

--- lua/config/plugin/notify/setup.lua
+++ lua/config/plugin/notify/setup.lua
@@ -1,17 +1,15 @@
-local notify = require("notify")
+local config = {
+  level = "TRACE",
+  timeout = 2500,
+  max_width = function()
+    return math.floor(math.max(vim.api.nvim_get_option("columns") / 3, 20))
+  end,
+  max_height = function()
+    return math.floor(math.max(vim.api.nvim_get_option("lines") / 3, 4))
+  end,
+}
+
 local sign = require("const.DIAGNOSTIC_SIGN")
-local config = {}
-
-config.max_width = function()
-  return math.floor(math.max(vim.api.nvim_get_option("columns") / 3, 20))
-end
-
-config.max_height = function()
-  return math.floor(math.max(vim.api.nvim_get_option("lines") / 3, 4))
-end
-
-config.level = "TRACE"
-config.timeout = 3000
 config.icons = {
   ERROR = sign.ERROR,
   WARN = sign.WARN,
@@ -19,5 +17,7 @@ config.icons = {
   DEBUG = "",
   TRACE = "",
 }
+
+local notify = require("notify")
 notify.setup(config)
 vim.notify = notify
