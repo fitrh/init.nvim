79a5ae9cf2f1088438fe275b42708ec343bec77a
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-11-25 23:16:40 +0800

    config.plugin.litee: Add more icons to icon_set_custom

--- lua/config/plugin/litee/setup.lua
+++ lua/config/plugin/litee/setup.lua
@@ -1,10 +1,38 @@
+local sym = require("const.LSP_KIND").Symbol
+local icon_set_custom = {
+  Array = sym.Array.icon,
+  Boolean = sym.Boolean.icon,
+  Class = sym.Class.icon,
+  Constant = sym.Constant.icon,
+  Constructor = sym.Constructor.icon,
+  Collapsed = "",
+  Enum = sym.Enum.icon,
+  EnumMember = sym.EnumMember.icon,
+  Event = sym.Event.icon,
+  Expanded = "",
+  Field = sym.Field.icon,
+  File = sym.File.icon,
+  Function = sym.Function.icon,
+  IndentGuide = " ",
+  Interface = sym.Interface.icon,
+  Key = sym.Key.icon,
+  Method = sym.Method.icon,
+  Module = sym.Module.icon,
+  Namespace = sym.Namespace.icon,
+  Null = sym.Null.icon,
+  Object = sym.Object.icon,
+  Operator = sym.Operator.icon,
+  Package = sym.Package.icon,
+  Property = sym.Property.icon,
+  String = sym.String.icon,
+  Struct = sym.Struct.icon,
+  TypeParameter = sym.TypeParameter.icon,
+  Variable = sym.Variable.icon,
+}
+
 require("litee.lib").setup({
   tree = {
-    icon_set_custom = {
-      Collapsed = "",
-      Expanded = "",
-      IndentGuide = " ",
-    },
+    icon_set_custom = icon_set_custom,
     icon_set = "nerd",
   },
   panel = {
@@ -14,6 +42,7 @@ require("litee.lib").setup({
 })
 
 require("litee.symboltree").setup({
+  icon_set_custom = icon_set_custom,
   icon_set = "nerd",
   hide_cursor = false,
   on_open = "panel",
@@ -21,6 +50,7 @@ require("litee.symboltree").setup({
 })
 
 require("litee.calltree").setup({
+  icon_set_custom = icon_set_custom,
   icon_set = "nerd",
   hide_cursor = false,
   on_open = "panel",
