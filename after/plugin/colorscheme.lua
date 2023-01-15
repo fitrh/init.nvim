a8fee709f4ea8c951396c4a38638b586709c34fb
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-01-15 23:57:40 +0800

    colorscheme: Use `User %ColorSchemeName%Pre` event for loading
    This changes decouple `after/plugin/colorscheme.lua` from lua module
    and plugin managers can lazy load the colorschemes using
    `User %ColorSchemeName%Pre` event.

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,21 +1,15 @@
-vim.api.nvim_create_autocmd("UIEnter", {
-  group = vim.api.nvim_create_augroup("set_colorscheme", {}),
-  callback = function()
-    local default = "tokyonight"
-    local colorschemes = {
-      ["doom-one"] = "doom-one",
-      github = "github",
-      gruvbox = "gruvbox",
-      kanagawa = "kanagawa",
-      material = "material",
-      ["rose-pine"] = "rose-pine",
-      tokyonight = "tokyonight",
-    }
+local default = "tokyonight"
+local colorschemes = {
+  ["doom-one"] = "DoomOnePre",
+  github = "GithubPre",
+  gruvbox = "GruvboxPre",
+  kanagawa = "KanagawaPre",
+  material = "MaterialPre",
+  ["rose-pine"] = "RosePinePre",
+  tokyonight = "TokyoNightPre",
+}
 
-    local colorscheme = colorschemes[os.getenv("NVIM_COLORSCHEME")] or default
-    vim.api.nvim_exec_autocmds("ColorSchemePre", {})
-    vim.cmd.packadd({ ("colorscheme/%s"):format(colorscheme), bang = true })
-    require(("config.plugin.colorscheme.%s.setup"):format(colorscheme))
-    vim.api.nvim_exec_autocmds("ColorScheme", { pattern = colorscheme })
-  end,
-})
+local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME") or default
+vim.api.nvim_exec_autocmds("ColorSchemePre", {})
+vim.api.nvim_exec_autocmds("User", { pattern = colorschemes[NVIM_COLORSCHEME] })
+vim.api.nvim_exec_autocmds("ColorScheme", { pattern = NVIM_COLORSCHEME })
--- lua/config/plugin/colorscheme/packerspec.lua
+++ lua/config/plugin/colorscheme/packerspec.lua
@@ -1,9 +1,58 @@
 return {
-  { "folke/tokyonight.nvim", as = "colorscheme/tokyonight", opt = true },
-  { "rebelot/kanagawa.nvim", as = "colorscheme/kanagawa", opt = true },
-  { "marko-cerovac/material.nvim", as = "colorscheme/material", opt = true },
-  { "projekt0n/github-nvim-theme", as = "colorscheme/github", opt = true },
-  { "rose-pine/neovim", as = "colorscheme/rose-pine", opt = true },
-  { "ellisonleao/gruvbox.nvim", as = "colorscheme/gruvbox", opt = true },
-  { "NTBBloodbath/doom-one.nvim", as = "colorscheme/doom-one", opt = true },
+  {
+    "folke/tokyonight.nvim",
+    as = "colorscheme/tokyonight",
+    config = function()
+      require("config.plugin.colorscheme.tokyonight.setup")
+    end,
+    event = { "User TokyoNightPre" },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    as = "colorscheme/kanagawa",
+    config = function()
+      require("config.plugin.colorscheme.kanagawa.setup")
+    end,
+    event = { "User KanagawaPre" },
+  },
+  {
+    "marko-cerovac/material.nvim",
+    as = "colorscheme/material",
+    config = function()
+      require("config.plugin.colorscheme.material.setup")
+    end,
+    event = { "User MaterialPre" },
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    as = "colorscheme/github",
+    config = function()
+      require("config.plugin.colorscheme.github.setup")
+    end,
+    event = { "User GithubPre" },
+  },
+  {
+    "rose-pine/neovim",
+    as = "colorscheme/rose-pine",
+    config = function()
+      require("config.plugin.colorscheme.rose-pine.setup")
+    end,
+    event = { "User RosePinePre" },
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    as = "colorscheme/gruvbox",
+    config = function()
+      require("config.plugin.colorscheme.gruvbox.setup")
+    end,
+    event = { "User GruvboxPre" },
+  },
+  {
+    "NTBBloodbath/doom-one.nvim",
+    as = "colorscheme/doom-one",
+    config = function()
+      require("config.plugin.colorscheme.doom-one.setup")
+    end,
+    event = { "User DoomOnePre" },
+  },
 }
