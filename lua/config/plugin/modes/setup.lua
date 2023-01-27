211c75265dc3b4743982e052d51ab6a375b19b31
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-01-27 23:42:22 +0800

    plugin.modes: Link `Modes*CursorLine*` to `Modes*CursorLineNr`

--- lua/config/plugin/modes/setup.lua
+++ lua/config/plugin/modes/setup.lua
@@ -4,3 +4,12 @@ config.line_opacity = { insert = 0, visual = 0.2 }
 config.set_cursorline = false
 
 require("modes").setup(config)
+
+for _, mode in ipairs({ "Insert", "Copy", "Visual", "Delete" }) do
+  local linenr = string.format("Modes%sCursorLineNr", mode)
+
+  for _, kind in ipairs({ "Fold", "Sign" }) do
+    local group = string.format("Modes%sCursorLine%s", mode, kind)
+    vim.api.nvim_set_hl(0, group, { link = linenr })
+  end
+end
