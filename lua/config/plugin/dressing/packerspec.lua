9e32b55f5934f3678c301b43238c593479489750
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-02-03 23:01:58 +0800

    plugin: Remove `stevearc/dressing.nvim` and `declancm/cinnamon.nvim`

--- lua/config/plugin/dressing/packerspec.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "stevearc/dressing.nvim",
-  requires = { "nvim-telescope/telescope.nvim", module = "telescope" },
-  config = function()
-    require("config.plugin.dressing.setup")
-  end,
-  event = { "BufReadPost", "BufNewFile" },
-}
