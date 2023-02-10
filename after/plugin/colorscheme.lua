5ce34d87bdd4cbd4d56e2634049803173ff5a7bd
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-02-10 22:04:14 +0800

    after/plugin/colorscheme: Put in `UIEnter` autocommand

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,21 +1,28 @@
-local default = "tokyonight"
-local colorschemes = {
-  ["doom-one"] = "DoomOnePre",
-  github = "GithubPre",
-  gruvbox = "GruvboxPre",
-  kanagawa = "KanagawaPre",
-  material = "MaterialPre",
-  ["rose-pine"] = "RosePinePre",
-  tokyonight = "TokyoNightPre",
-}
+local a = vim.api
 
-local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME")
-local colorscheme = colorschemes[NVIM_COLORSCHEME]
-if not colorscheme then
-  NVIM_COLORSCHEME = default
-  colorscheme = colorschemes[NVIM_COLORSCHEME]
-end
+a.nvim_create_autocmd("UIEnter", {
+  group = a.nvim_create_augroup("set_colorscheme", {}),
+  callback = function()
+    local default = "tokyonight"
+    local colorschemes = {
+      ["doom-one"] = "DoomOnePre",
+      github = "GithubPre",
+      gruvbox = "GruvboxPre",
+      kanagawa = "KanagawaPre",
+      material = "MaterialPre",
+      ["rose-pine"] = "RosePinePre",
+      tokyonight = "TokyoNightPre",
+    }
 
-vim.api.nvim_exec_autocmds("ColorSchemePre", {})
-vim.api.nvim_exec_autocmds("User", { pattern = colorscheme })
-vim.api.nvim_exec_autocmds("ColorScheme", { pattern = NVIM_COLORSCHEME })
+    local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME")
+    local colorscheme = colorschemes[NVIM_COLORSCHEME]
+    if not colorscheme then
+      NVIM_COLORSCHEME = default
+      colorscheme = colorschemes[NVIM_COLORSCHEME]
+    end
+
+    a.nvim_exec_autocmds("ColorSchemePre", {})
+    a.nvim_exec_autocmds("User", { pattern = colorscheme })
+    a.nvim_exec_autocmds("ColorScheme", { pattern = NVIM_COLORSCHEME })
+  end,
+})
