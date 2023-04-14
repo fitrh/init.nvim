86c8d90e6c1469b61645a466ec1f5f667f05a4f3
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-04-14 23:27:07 +0800

    plugin: Remove `folke/trouble.nvim`

--- after/ftplugin/Trouble.lua
+++
@@ -1,5 +0,0 @@
-local b = vim.b
-b.keep_cursor_on_leave = true
-
-local set = vim.opt_local
-set.colorcolumn = ""
--- lua/config/plugin/trouble/setup.lua
+++
@@ -1,4 +0,0 @@
-require("trouble").setup({
-  auto_preview = false,
-  use_diagnostic_signs = true,
-})
