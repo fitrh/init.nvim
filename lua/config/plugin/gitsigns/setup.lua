52ccc64fb0cb0137527723bfb7ea4b06d6b9350e
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-05-26 20:47:37 +0800

    plugin.gitsigns: Add space at the end of current line blame
    Makes virtual text look good if it has a background color.

--- lua/config/plugin/gitsigns/setup.lua
+++ lua/config/plugin/gitsigns/setup.lua
@@ -40,6 +40,6 @@ gitsigns.setup({
   diff_opts = { internal = true, linematch = 60 },
   preview_config = { border = "rounded", focusable = false },
   current_line_blame_opts = { delay = vim.api.nvim_get_option("updatetime") },
-  current_line_blame_formatter = " ﰖ <abbrev_sha> by <author>, <author_time:%R> - <summary>",
+  current_line_blame_formatter = " ﰖ <abbrev_sha> by <author>, <author_time:%R> - <summary> ",
   trouble = false,
 })
