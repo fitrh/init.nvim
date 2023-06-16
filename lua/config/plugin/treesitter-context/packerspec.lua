b7f4d53230376138dd0dd32b732f070e266a5b57
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-06-16 21:36:24 +0800

    plugin.treesitter-context.packerspec: Remove dependency on `nvim-treesitter`
    Ref: https://github.com/nvim-treesitter/nvim-treesitter-context/commit/b9f734e

--- lua/config/plugin/treesitter-context/packerspec.lua
+++ lua/config/plugin/treesitter-context/packerspec.lua
@@ -1,9 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   after = "nvim-treesitter",
-  requires = {
-    { "nvim-treesitter/nvim-treesitter", module = "nvim-treesitter" },
-  },
   config = function()
     require("treesitter-context").setup({
       enable = false,
