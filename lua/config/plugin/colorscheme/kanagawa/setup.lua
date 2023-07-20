b1a2a35368a9fcd7c7277fc3a163533191c67daa
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-07-20 22:58:51 +0800

    colorscheme(kanagawa): Adapt to upstream overhaul
    Ref: https://github.com/rebelot/kanagawa.nvim/pull/101

--- lua/config/plugin/colorscheme/kanagawa/setup.lua
+++ lua/config/plugin/colorscheme/kanagawa/setup.lua
@@ -1,40 +1,45 @@
-local function get_mode()
-  local modes = { dark = "default", light = "light" }
-  return modes[os.getenv("NVIM_KANAGAWA_MODE")] or "default"
+local function get_variant()
+  local variants = { wave = "wave", dragon = "dragon", lotus = "lotus" }
+  return variants[os.getenv("NVIM_KANAGAWA_VARIANT")] or "wave"
 end
 
-local config = {}
+local function get_mode()
+  local modes = { dark = "wave", light = "lotus" }
+  return modes[os.getenv("NVIM_KANAGAWA_MODE")] or "wave"
+end
 
-config.globalStatus = true
-config.theme = get_mode()
+local config = {
+  theme = get_mode() == "lotus" and "lotus" or get_variant(),
+}
 
-require("kanagawa").setup(config)
-
-vim.cmd.colorscheme("kanagawa")
-
----@type KanagawaColors|ThemeColors
-local c = require("kanagawa.colors").setup(config)
+local background = config.theme == "lotus" and "light" or "dark"
+vim.api.nvim_set_option_value("background", background, {})
+local kanagawa = require("kanagawa")
+kanagawa.setup(config)
+kanagawa.load(config.theme)
 
 require("sugar.highlight").colorscheme(function(h)
   local set, link, fg, bg, blend = h.set, h.link, h.fg, h.bg, h.blend
   local fmt = string.format
 
   local base = bg("Normal")
+  ---@type ThemeColors
+  local c = require("kanagawa.colors").setup(config).theme
 
   -- highlight-default
-  set("ColorColumn", { bg = blend(bg("StatusLine"), bg("Normal"), 0.3) })
-  set("CursorColumn", { bg = blend(bg("CursorLine"), bg("Normal"), 0.2) })
+  set("ColorColumn", { bg = blend(bg("StatusLine"), base, 0.3) })
+  set("CursorColumn", { bg = blend(bg("CursorLine"), base, 0.3) })
   link("CursorLine", "CursorColumn")
+  set("CursorLineNr", { inherit = "CursorLineNr", bg = "NONE", bold = false })
   set("FloatBorder", { inherit = "NormalFloat", fg = bg("NormalFloat") })
   set("Folded", { inherit = "Folded", bg = "NONE" })
+  set("LineNr", { inherit = "LineNr", bg = "NONE" })
   link("MatchParen", "LspReferenceText")
   link("MsgArea", "StatusLine")
   set("NonText", { inherit = "NonText", bold = false })
-  set("TabLine", { inherit = "StatusLine", fg = c.fujiGray })
+  link("TabLine", "StatusLine")
   link("TabLineFill", "TabLine")
   link("TabLineSel", "Normal")
-  set("VertSplit", { fg = c.fg_border })
-  link("WinSeparator", "VertSplit")
 
   -- treesitter-highlight-groups
   link("@text.diff.add", "diffAdded")
@@ -44,19 +49,20 @@ require("sugar.highlight").colorscheme(function(h)
   link("CmpCursorLine", "PmenuSel")
   set("CmpDoc", { inherit = "PmenuSel", blend = 0 })
   link("FloatTitle", "Title")
-  set("HopNextKey", { fg = c.peachRed, bold = true })
-  set("HopNextKey1", { fg = c.springGreen, bold = true })
-  set("HopNextKey2", { fg = c.waveBlue2 })
-  set("HopUnmatched", { fg = c.sumiInk4, special = c.sumiInk4 })
+  set("HopNextKey", { fg = c.syn.special3, bold = true })
+  set("HopNextKey1", { fg = c.syn.string, bold = true })
+  set("HopNextKey2", { fg = c.ui.bg_search })
+  set("HopUnmatched", { fg = c.ui.nontext, special = c.ui.nontext })
   link("InclineNormal", "StatusLine")
-  set("InclineNormalNC", { inherit = "StatusLineNC", fg = c.sumiInk4 })
+  set("InclineNormalNC", { inherit = "StatusLineNC", fg = c.ui.whitespace })
   set("InclineSep", { fg = fg("LineNr"), bold = true })
-  set("InclineTail", { fg = c.springViolet1, bold = true })
+  set("InclineTail", { fg = c.ui.special, bold = true })
+  local fg_cul_nr = fg("CursorLineNr")
   set("InclineWinNr", {
-    fg = fg("CursorLineNr"),
-    bg = blend(fg("CursorLineNr"), bg("StatusLine"), 0.1),
+    fg = fg_cul_nr,
+    bg = blend(fg_cul_nr, bg("StatusLine"), 0.1),
   })
-  set("LTSymbol", { fg = c.oldWhite })
+  set("LTSymbol", { fg = c.ui.fg_dim })
   link("LTSymbolDetail", "Comment")
   link("LTSymbolJump", "LspReferenceText")
   link("LTBoolean", "@boolean")
@@ -74,13 +80,13 @@ require("sugar.highlight").colorscheme(function(h)
   link("LTType", "@type")
   link("LTURI", "@text.uri")
   set("ModesCopy", { bg = bg("IncSearch") })
-  set("ModesCopyCursorLineNr", { fg = bg("ModesCopy") })
-  set("ModesDelete", { bg = c.autumnRed })
-  set("ModesDeleteCursorLineNr", { fg = bg("ModesDelete") })
-  set("ModesInsert", { bg = c.autumnGreen })
-  set("ModesInsertCursorLineNr", { fg = bg("ModesInsert") })
-  set("ModesVisual", { bg = c.oniViolet })
-  set("ModesVisualCursorLineNr", { fg = bg("ModesVisual") })
+  set("ModesDelete", { bg = c.vcs.removed })
+  set("ModesInsert", { bg = c.vcs.added })
+  set("ModesVisual", { bg = c.syn.statement })
+  for _, v in ipairs({ "Copy", "Delete", "Insert", "Visual" }) do
+    local def = { inherit = "CursorLineNr", fg = bg(concat({ "Modes", v })) }
+    set(concat({ "Modes", v, "CursorLineNr" }), def)
+  end
 
   --- github.com/rcarriga/nvim-notify
   for _, v in ipairs({ "TRACE", "DEBUG", "INFO", "WARN", "ERROR" }) do
@@ -90,19 +96,19 @@ require("sugar.highlight").colorscheme(function(h)
     set(fmt("Notify%sBody", v), { inherit = title, bg = color })
   end
 
-  set("StatusLineDim", { inherit = "StatusLine", fg = c.boatYellow1 })
-  set("StatusLineGitBranch", { inherit = "StatusLine", fg = c.springViolet1 })
-  set("StatusLineGitDiffAdd", { inherit = "StatusLine", fg = c.git.added })
-  set("StatusLineGitDiffChange", { inherit = "StatusLine", fg = c.git.changed })
-  set("StatusLineGitDiffDelete", { inherit = "StatusLine", fg = c.git.removed })
-  set("StatusLineModified", { inherit = "StatusLine", fg = c.autumnRed })
-  set("StatusLinePath", { inherit = "StatusLine", fg = fg("Comment") })
+  set("StatusLineDim", { inherit = "StatusLine", fg = c.ui.nontext })
+  set("StatusLineGitBranch", { inherit = "StatusLine", fg = c.ui.special })
+  set("StatusLineGitDiffAdd", { inherit = "StatusLine", fg = c.vcs.added })
+  set("StatusLineGitDiffChange", { inherit = "StatusLine", fg = c.vcs.changed })
+  set("StatusLineGitDiffDelete", { inherit = "StatusLine", fg = c.vcs.removed })
+  set("StatusLineModified", { inherit = "StatusLine", fg = c.vcs.removed })
+  set("StatusLinePath", { inherit = "StatusLine", fg = c.diff.text })
   set("StatusLinePathSep", { inherit = "StatusLineDim", bold = true })
-  set("StatusLineRO", { inherit = "StatusLine", fg = c.samuraiRed })
-  set("TabLineModified", { inherit = "TabLine", fg = c.autumnRed })
+  set("StatusLineRO", { inherit = "StatusLine", fg = c.diag.error })
+  set("TabLineModified", { inherit = "TabLine", fg = c.vcs.added })
   link("TabLineSep", "TabLine")
-  set("TabLineModifiedSel", { inherit = "TabLineSel", fg = c.autumnRed })
-  set("TabLineSepSel", { inherit = "TabLineSel", fg = c.crystalBlue })
+  set("TabLineModifiedSel", { inherit = "TabLineSel", fg = c.vcs.added })
+  set("TabLineSepSel", { inherit = "TabLineSel", fg = c.syn.fun })
   set("TelescopeNormal", { bg = bg("StatusLine") })
   link("TelescopePreviewLine", "CursorLine")
   link("TelescopeSelection", "CursorLine")
@@ -111,7 +117,7 @@ require("sugar.highlight").colorscheme(function(h)
   set("TreesitterContext", { inherit = "ColorColumn", bold = true })
   set("TreesitterContextLineNumber", {
     inherit = "TreesitterContext",
-    fg = c.fg,
+    fg = c.ui.fg,
   })
-  set("WinSeparatorZen", { fg = bg("Normal") })
+  set("WinSeparatorZen", { fg = base })
 end)
