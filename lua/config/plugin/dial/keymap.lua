907a9b4ab0c74ce47d6871ab6e5261588c73917e
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-11-24 20:30:22 +0800

    plugin.dial.keymap: Use lua API

--- lua/config/plugin/dial/keymap.lua
+++ lua/config/plugin/dial/keymap.lua
@@ -1,8 +1,45 @@
-local map = vim.keymap.set
+local nmap, xmap = {}, {}
 
-map({ "n" }, "<C-a>", "<Plug>(dial-increment)", { silent = true })
-map({ "n" }, "<C-x>", "<Plug>(dial-decrement)", { silent = true })
-map({ "v" }, "<C-a>", "<Plug>(dial-increment)", { silent = true })
-map({ "v" }, "<C-x>", "<Plug>(dial-decrement)", { silent = true })
-map({ "v" }, "g<C-a>", "g<Plug>(dial-increment)", { silent = true })
-map({ "v" }, "g<C-x>", "g<Plug>(dial-decrement)", { silent = true })
+nmap["<C-a>"] = function()
+  require("dial.map").manipulate("increment", "normal")
+end
+nmap["<C-x>"] = function()
+  require("dial.map").manipulate("decrement", "normal")
+end
+nmap["g<C-a>"] = function()
+  require("dial.map").manipulate("increment", "gnormal")
+end
+nmap["g<C-x>"] = function()
+  require("dial.map").manipulate("decrement", "gnormal")
+end
+
+xmap["<C-a>"] = function()
+  require("dial.map").manipulate("increment", "visual")
+end
+xmap["<C-x>"] = function()
+  require("dial.map").manipulate("decrement", "visual")
+end
+xmap["g<C-a>"] = function()
+  require("dial.map").manipulate("increment", "gvisual")
+end
+xmap["g<C-x>"] = function()
+  require("dial.map").manipulate("decrement", "gvisual")
+end
+
+local map = vim.api.nvim_set_keymap
+
+for key, fn in pairs(nmap) do
+  map("n", key, "", {
+    callback = fn,
+    silent = true,
+    noremap = true,
+  })
+end
+
+for key, fn in pairs(xmap) do
+  map("x", key, "", {
+    callback = fn,
+    silent = true,
+    noremap = true,
+  })
+end
