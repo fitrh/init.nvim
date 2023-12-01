f343c428ec81ed38bb98c02ee80857a43d85e347
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-12-01 20:25:00 +0800

    plugin.kommentary: Add `ts-context-commentstring` integration

--- lua/config/plugin/kommentary/setup.lua
+++ lua/config/plugin/kommentary/setup.lua
@@ -3,6 +3,16 @@ local on_lang = config.configure_language
 
 config.use_extended_mappings()
 
+on_lang("default", {
+  single_line_comment_string = "auto",
+  multi_line_comment_strings = "auto",
+  hook_function = function()
+    if package.loaded["ts_context_commentstring"] then
+      require("ts_context_commentstring.internal").update_commentstring()
+    end
+  end,
+})
+
 on_lang("c", {
   prefer_multi_line_comments = true,
 })
--- lua/config/plugin/ts-context-commentstring/packerspec.lua
+++ lua/config/plugin/ts-context-commentstring/packerspec.lua
@@ -1,6 +1,9 @@
 return {
   "JoosepAlviste/nvim-ts-context-commentstring",
   after = "nvim-treesitter",
+  config = function()
+    require("config.plugin.ts-context-commentstring.setup")
+  end,
   setup = function()
     vim.api.nvim_set_var("skip_ts_context_commentstring_module", true)
   end,
