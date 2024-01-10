49c8214df0a53bf75e5274b0e792ff407ef2327b
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2024-01-10 23:22:59 +0800

    variable: Replace `vim.g` winth `vim.api.nvim_set_var`

--- lua/config/variable.lua
+++ lua/config/variable.lua
@@ -1,27 +1,26 @@
-local g = vim.g
-
-g.mapleader = " "
-g.loaded_python3_provider = 0
-g.loaded_python_provider = 0
-g.loaded_ruby_provider = 0
-g.loaded_node_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_spec = 1
-g.loaded_gzip = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_2html_plugin = 1
-g.loaded_logiPat = 1
-g.loaded_rrhelper = 1
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
-g.loaded_netrwFileHandlers = 1
+local g = vim.api.nvim_set_var
+g("mapleader", " ")
+g("loaded_python3_provider", 0)
+g("loaded_python_provider", 0)
+g("loaded_ruby_provider", 0)
+g("loaded_node_provider", 0)
+g("loaded_perl_provider", 0)
+g("loaded_spec", 1)
+g("loaded_gzip", 1)
+g("loaded_tar", 1)
+g("loaded_tarPlugin", 1)
+g("loaded_zip", 1)
+g("loaded_zipPlugin", 1)
+g("loaded_getscript", 1)
+g("loaded_getscriptPlugin", 1)
+g("loaded_vimball", 1)
+g("loaded_vimballPlugin", 1)
+g("loaded_matchit", 1)
+g("loaded_matchparen", 1)
+g("loaded_2html_plugin", 1)
+g("loaded_logiPat", 1)
+g("loaded_rrhelper", 1)
+g("loaded_netrw", 1)
+g("loaded_netrwPlugin", 1)
+g("loaded_netrwSettings", 1)
+g("loaded_netrwFileHandlers", 1)
