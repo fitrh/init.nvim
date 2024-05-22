5fba0bbc6b61135e7e66549471f971a52cd243a1
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2024-05-22 23:19:16 +0800

    after/plugin/colorscheme: Use default colorscheme
    Since the default colorscheme does not trigger the `ColorScheme` event,
    we need to trigger it manually to be able to customize the default colorscheme
    using autocmd

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -3,7 +3,14 @@ local a = vim.api
 a.nvim_create_autocmd("UIEnter", {
   group = a.nvim_create_augroup("set_colorscheme", {}),
   callback = function()
-    local default = "tokyonight"
+    local env = os.getenv
+
+    local colorscheme = env("NVIM_COLORSCHEME")
+    if not colorscheme then
+      a.nvim_exec_autocmds("ColorScheme", { pattern = "default" })
+      return true
+    end
+
     local colorschemes = {
       ["doom-one"] = "DoomOnePre",
       github = "GithubPre",
@@ -13,17 +20,15 @@ a.nvim_create_autocmd("UIEnter", {
       ["rose-pine"] = "RosePinePre",
       tokyonight = "TokyoNightPre",
     }
-
-    local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME")
-    local colorscheme = colorschemes[NVIM_COLORSCHEME]
-    if not colorscheme then
-      NVIM_COLORSCHEME = default
-      colorscheme = colorschemes[NVIM_COLORSCHEME]
+    local color_event = colorschemes[colorscheme]
+    if not color_event then
+      a.nvim_exec_autocmds("ColorScheme", { pattern = "default" })
+      return true
     end
 
     a.nvim_exec_autocmds("ColorSchemePre", {})
-    a.nvim_exec_autocmds("User", { pattern = colorscheme })
-    a.nvim_exec_autocmds("ColorScheme", { pattern = NVIM_COLORSCHEME })
+    a.nvim_exec_autocmds("User", { pattern = color_event })
+    a.nvim_exec_autocmds("ColorScheme", { pattern = colorscheme })
   end,
   once = true,
 })
